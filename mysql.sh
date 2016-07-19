51dab6fab0ecf70e588125378bfae80cae9fb1b7
あまみや ゆうこ <1@1234.sh>
2016-07-19 11:02:47 +0800

    Fixed mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget http://dev.mysql.com/get/mysql-apt-config_0.3.2-1ubuntu14.04_all.deb
-sudo dpkg -i mysql-apt-config_0.3.2-1ubuntu14.04_all.deb
+wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
+sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
 sudo apt-get update
 sudo apt-get install mysql-server-5.7
