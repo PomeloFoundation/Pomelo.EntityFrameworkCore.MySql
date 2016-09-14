25f3adcbec116e0d92bb06c623c1547bbf07832c
Caleb Lloyd <caleblloyd@gmail.com>
2016-09-13 22:27:49 -0400

    functional testing app shell

---
+++ test/Pomelo.EntityFrameworkCore.MySql.Functional/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
