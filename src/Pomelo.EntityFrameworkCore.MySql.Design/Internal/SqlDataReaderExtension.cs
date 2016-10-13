d7a9fe814c65dd1cdf66b44424bd0f98fafbf60b
雨宮丶優子 <1@1234.sh>
2016-10-13 16:14:56 +0800

    Design time supports

---
+++ src/Pomelo.EntityFrameworkCore.MySql.Design/Internal/SqlDataReaderExtension.cs
@@ -0,0 +1,29 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using MySql.Data.MySqlClient;
+using JetBrains.Annotations;
+
+namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
+{
+    // TODO merge with DbDataReaderExtension.GetValueOrDefault when Mono supports GetFieldValue. See #2079
+    /// <summary>
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public static class SqlDataReaderExtension
+    {
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public static T GetValueOrDefault<T>([NotNull]this MySqlDataReader reader, [NotNull]string name)
+        {
+            var idx = reader.GetOrdinal(name);
+            return reader.IsDBNull(idx)
+                ? default(T)
+                // : reader.GetFieldValue<T>(idx);
+                : (T)reader.GetValue(idx);
+        }
+    }
+}
