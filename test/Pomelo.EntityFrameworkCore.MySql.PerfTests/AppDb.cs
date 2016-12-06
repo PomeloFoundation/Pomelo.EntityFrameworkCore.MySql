db6e6ed463cc39a63048f7451fea0fd1e6450f18
caleblloyd <caleblloyd@gmail.com>
2016-12-05 22:24:20 -0500

    default string key length 127 (#139)

--- test/Pomelo.EntityFrameworkCore.MySql.PerfTests/AppDb.cs
+++ test/Pomelo.EntityFrameworkCore.MySql.PerfTests/AppDb.cs
@@ -63,27 +63,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.PerfTests
 		{
 			base.OnModelCreating(modelBuilder);
 
-			// Shorten key length for Identity
-			modelBuilder.Entity<AppIdentityUser>(entity => entity.Property(m => m.Id).HasMaxLength(127));
-			modelBuilder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(127));
-			modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
-			{
-				entity.Property(m => m.LoginProvider).HasMaxLength(127);
-				entity.Property(m => m.ProviderKey).HasMaxLength(127);
-			});
-			modelBuilder.Entity<IdentityUserRole<string>>(entity =>
-			{
-				entity.Property(m => m.UserId).HasMaxLength(127);
-				entity.Property(m => m.RoleId).HasMaxLength(127);
-			});
-			modelBuilder.Entity<IdentityUserToken<string>>(entity =>
-			{
-				entity.Property(m => m.UserId).HasMaxLength(127);
-				entity.Property(m => m.LoginProvider).HasMaxLength(127);
-				entity.Property(m => m.Name).HasMaxLength(127);
-
-			});
-
 			// Add our models fluent APIs
 			CrmMeta.OnModelCreating(modelBuilder);
 			GeneratedContactMeta.OnModelCreating(modelBuilder);
