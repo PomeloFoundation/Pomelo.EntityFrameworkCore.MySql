031e83139d4e6c51b3a510de4e0f2dbdf4fa1636
Hu Weiwen <huww98@outlook.com>
2017-03-23 23:09:30 +0800

    fix issue #196 (#220)
    * fix issue #196
    
    * Add all spatial types.

--- test/Pomelo.EntityFrameworkCore.MySql.Tests/Migrations/MigrationSqlGeneratorTest.cs
+++ test/Pomelo.EntityFrameworkCore.MySql.Tests/Migrations/MigrationSqlGeneratorTest.cs
@@ -388,6 +388,44 @@ END;" + EOL +
             Sql, false , true, true);
         }
 
+        [Theory]
+        [InlineData("tinyblob")]
+        [InlineData("blob")]
+        [InlineData("mediumblob")]
+        [InlineData("longblob")]
+
+        [InlineData("tinytext")]
+        [InlineData("text")]
+        [InlineData("mediumtext")]
+        [InlineData("longtext")]
+
+        [InlineData("geometry")]
+        [InlineData("point")]
+        [InlineData("linestring")]
+        [InlineData("polygon")]
+        [InlineData("multipoint")]
+        [InlineData("multilinestring")]
+        [InlineData("multipolygon")]
+        [InlineData("geometrycollection")]
+
+        [InlineData("json")]
+        public void AlterColumnOperation_with_no_default_value_column_types(string type)
+        {
+            Generate(
+                new AlterColumnOperation
+                {
+                    Table = "People",
+                    Name = "Blob",
+                    ClrType = typeof(string),
+                    ColumnType = type,
+                    IsNullable = true,
+                });
+
+            Assert.Equal(
+                $"ALTER TABLE `People` MODIFY COLUMN `Blob` {type} NULL;" + EOL,
+                Sql);
+        }
+
         #endregion
 
         #region Npgsql-specific
