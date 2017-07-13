3a085a6b8e31ae142c163e41d589daafcd6ae267
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-13 11:40:42 +0200

    Expose internals to unit tests

--- src/EFCore.MySql/Properties/AssemblyInfo.cs
+++ src/EFCore.MySql/Properties/AssemblyInfo.cs
@@ -2,9 +2,11 @@
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Design;
 
 [assembly: AssemblyTitle("Pomelo.EntityFrameworkCore.MySql")]
 [assembly: AssemblyDescription("MySQL provider for Entity Framework Core")]
 [assembly: DesignTimeProviderServices("Microsoft.EntityFrameworkCore.Design.Internal.MySqlDesignTimeServices")]
+[assembly: InternalsVisibleTo("Pomelo.EntityFrameworkCore.MySql.Tests")]
