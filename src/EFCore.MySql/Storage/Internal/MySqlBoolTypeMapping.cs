f4d48219ebaef321561e5a53dfc7458209b1b7b0
あまみや ゆうこ <1@1234.sh>
2017-07-27 22:31:41 +0800

    Fix mapping

---
+++ src/EFCore.MySql/Storage/Internal/MySqlBoolTypeMapping.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Pomelo Foundation. All rights reserved.
+// Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System.Data;
+using JetBrains.Annotations;
+
+namespace Microsoft.EntityFrameworkCore.Storage
+{
+    /// <summary>
+    ///     <para>
+    ///         Represents the mapping between a .NET <see cref="bool" /> type and a database type.
+    ///     </para>
+    ///     <para>
+    ///         This type is typically used by database providers (and other extensions). It is generally
+    ///         not used in application code.
+    ///     </para>
+    /// </summary>
+    public class MySqlBoolTypeMapping : RelationalTypeMapping<bool>
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BoolTypeMapping" /> class.
+        /// </summary>
+        /// <param name="storeType"> The name of the database type. </param>
+        /// <param name="dbType"> The <see cref="DbType" /> to be used. </param>
+        public MySqlBoolTypeMapping(
+            [NotNull] string storeType,
+            [CanBeNull] DbType? dbType = null)
+            : base(storeType, dbType, unicode: false, size: null)
+        {
+        }
+
+        /// <summary>
+        ///     Creates a copy of this mapping.
+        /// </summary>
+        /// <param name="storeType"> The name of the database type. </param>
+        /// <param name="size"> The size of data the property is configured to store, or null if no size is configured. </param>
+        /// <returns> The newly created mapping. </returns>
+        public override RelationalTypeMapping CreateCopy(string storeType, int? size)
+            => new BoolTypeMapping(storeType, DbType);
+
+        /// <summary>
+        ///     Generates the SQL representation of a literal value.
+        /// </summary>
+        /// <param name="value">The literal value.</param>
+        /// <returns>
+        ///     The generated string.
+        /// </returns>
+        protected override string GenerateNonNullSqlLiteral(object value)
+            => (bool)value ? "TRUE" : "FALSE";
+    }
+}
