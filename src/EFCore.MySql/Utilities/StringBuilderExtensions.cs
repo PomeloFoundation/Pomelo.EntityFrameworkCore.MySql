412e597237a9ba67c9450173e300c20b2978da36
Caleb Lloyd <caleblloyd@gmail.com>
2017-10-22 10:06:28 -0400

    port Update code to reflect SQL Server 2.0.1

--- src/EFCore.MySql/Utilities/StringBuilderExtensions.cs
+++ src/EFCore.MySql/Utilities/StringBuilderExtensions.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 
-// ReSharper disable once CheckNamespace
 namespace System.Text
 {
     internal static class StringBuilderExtensions
@@ -12,15 +11,70 @@ namespace System.Text
             this StringBuilder stringBuilder, IEnumerable<string> values, string separator = ", ")
             => stringBuilder.AppendJoin(values, (sb, value) => sb.Append(value), separator);
 
+        public static StringBuilder AppendJoin(
+            this StringBuilder stringBuilder, string separator, params string[] values)
+            => stringBuilder.AppendJoin(values, (sb, value) => sb.Append(value), separator);
+
         public static StringBuilder AppendJoin<T>(
-            this StringBuilder stringBuilder, IEnumerable<T> values, Action<StringBuilder, T> joinAction,
-            string separator)
+            this StringBuilder stringBuilder,
+            IEnumerable<T> values,
+            Action<StringBuilder, T> joinAction,
+            string separator = ", ")
         {
             var appended = false;
 
             foreach (var value in values)
             {
                 joinAction(stringBuilder, value);
+                stringBuilder.Append(separator);
+                appended = true;
+            }
+
+            if (appended)
+            {
+                stringBuilder.Length -= separator.Length;
+            }
+
+            return stringBuilder;
+        }
+
+        public static StringBuilder AppendJoin<T, TParam>(
+            this StringBuilder stringBuilder,
+            IEnumerable<T> values,
+            TParam param,
+            Action<StringBuilder, T, TParam> joinAction,
+            string separator = ", ")
+        {
+            var appended = false;
+
+            foreach (var value in values)
+            {
+                joinAction(stringBuilder, value, param);
+                stringBuilder.Append(separator);
+                appended = true;
+            }
+
+            if (appended)
+            {
+                stringBuilder.Length -= separator.Length;
+            }
+
+            return stringBuilder;
+        }
+
+        public static StringBuilder AppendJoin<T, TParam1, TParam2>(
+            this StringBuilder stringBuilder,
+            IEnumerable<T> values,
+            TParam1 param1,
+            TParam2 param2,
+            Action<StringBuilder, T, TParam1, TParam2> joinAction,
+            string separator = ", ")
+        {
+            var appended = false;
+
+            foreach (var value in values)
+            {
+                joinAction(stringBuilder, value, param1, param2);
                 stringBuilder.Append(separator);
                 appended = true;
             }
