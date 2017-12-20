f334197883197c0edfe10346e6744e6b1b0a3b85
Conrad Micallef <conrad@xcreds.com>
2017-12-20 08:49:14 +0100

    Select for Update branch

--- src/EFCore.MySql/Query/Sql/Internal/MySqlQuerySqlGeneratorFactory.cs
+++ src/EFCore.MySql/Query/Sql/Internal/MySqlQuerySqlGeneratorFactory.cs
@@ -1,7 +1,8 @@
-﻿// Copyright (c) Pomelo Foundation. All rights reserved.
+// Copyright (c) Pomelo Foundation. All rights reserved.
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 using Microsoft.EntityFrameworkCore.Query.Expressions;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Utilities;
@@ -11,14 +12,19 @@ namespace Microsoft.EntityFrameworkCore.Query.Sql.Internal
 {
     public class MySqlQuerySqlGeneratorFactory : QuerySqlGeneratorFactoryBase
     {
-        public MySqlQuerySqlGeneratorFactory([NotNull] QuerySqlGeneratorDependencies dependencies)
+        private readonly IMySqlOptions options;
+
+        public MySqlQuerySqlGeneratorFactory([NotNull] QuerySqlGeneratorDependencies dependencies,
+                                             [NotNull] IMySqlOptions options)
             : base(dependencies)
         {
+            this.options = options;
         }
 
         public override IQuerySqlGenerator CreateDefault(SelectExpression selectExpression)
             => new MySqlQuerySqlGenerator(
                 Dependencies,
-                Check.NotNull(selectExpression, nameof(selectExpression)));
+                Check.NotNull(selectExpression, nameof(selectExpression)),
+                options);
     }
 }
