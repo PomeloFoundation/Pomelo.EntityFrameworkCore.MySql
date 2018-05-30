199fdc4dcd0bd32737b2ddcde4e3c1b8a0a214f7
Caleb Lloyd <caleblloyd@gmail.com>
2018-05-30 00:16:56 -0400

    legacy migrations wip

--- src/EFCore.MySql/Extensions/MySqlModelBuilderExtensions.cs
+++ src/EFCore.MySql/Extensions/MySqlModelBuilderExtensions.cs
@@ -1,5 +1,5 @@
-// Copyright (c) Pomelo Foundation. All rights reserved.
-// Licensed under the MIT. See LICENSE in the project root for license information.
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -9,14 +9,14 @@ using Microsoft.EntityFrameworkCore.Utilities;
 namespace Microsoft.EntityFrameworkCore
 {
     /// <summary>
-    ///     MySQL specific extension methods for <see cref="ModelBuilder" />.
+    ///     MySql specific extension methods for <see cref="ModelBuilder" />.
     /// </summary>
     public static class MySqlModelBuilderExtensions
     {
         /// <summary>
-        ///     Configures the model to use the MySQL IDENTITY feature to generate values for key properties
-        ///     marked as <see cref="ValueGenerated.OnAdd" />, when targeting MySQL. This is the default
-        ///     behavior when targeting MySQL.
+        ///     Configures the model to use the MySql IDENTITY feature to generate values for key properties
+        ///     marked as <see cref="ValueGenerated.OnAdd" />, when targeting MySql. This is the default
+        ///     behavior when targeting MySql.
         /// </summary>
         /// <param name="modelBuilder"> The model builder. </param>
         /// <returns> The same builder instance so that multiple calls can be chained. </returns>
@@ -32,6 +32,13 @@ namespace Microsoft.EntityFrameworkCore
             return modelBuilder;
         }
 
+        /// <summary>
+        ///     Configures the model to use the MySql Computed feature to generate values for key properties
+        ///     marked as <see cref="ValueGenerated.OnAddOrUpdate" />, when targeting MySql. This is the default
+        ///     behavior when targeting MySql.
+        /// </summary>
+        /// <param name="modelBuilder"> The model builder. </param>
+        /// <returns> The same builder instance so that multiple calls can be chained. </returns>
         public static ModelBuilder ForMySqlUseComputedColumns(
             [NotNull] this ModelBuilder modelBuilder)
         {
