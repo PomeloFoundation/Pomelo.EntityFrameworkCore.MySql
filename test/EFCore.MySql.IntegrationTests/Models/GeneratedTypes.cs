cae9612a547ba485bd5ad85581b4cea561a589b8
Bart Dekker <bart@bataviagroep.nl>
2019-04-27 12:44:06 +0200

    - MariaDb fix for unsupported JSON unquoting extraction operator.

--- test/EFCore.MySql.IntegrationTests/Models/GeneratedTypes.cs
+++ test/EFCore.MySql.IntegrationTests/Models/GeneratedTypes.cs
@@ -3,106 +3,109 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 
 namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Models
 {
-	public static class GeneratedContactMeta
-	{
-		public static void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			// AppVeyor's MySQL version does not support these functions properly
-			if (AppConfig.AppVeyor)
-				return;
+    public static class GeneratedContactMeta
+    {
+        public static void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // AppVeyor's MySQL version and MariaDb's MySQL don't support these functions
+            if (AppConfig.AppVeyor || new ServerVersion(AppConfig.Config["Data:ServerVersion"]).Type == Infrastructure.ServerType.MariaDb)
+            {
+                return;
+            }
 
-			modelBuilder.Entity<GeneratedContact>(entity =>
-			{
-				entity.HasIndex(m => m.Name);
-				entity.HasIndex(m => m.Email);
+            modelBuilder.Entity<GeneratedContact>(entity =>
+            {
+                entity.HasIndex(m => m.Name);
+                entity.HasIndex(m => m.Email);
 
-				entity.Property(m => m.Name)
-					.ValueGeneratedOnAddOrUpdate()
-					.HasColumnType(@"VARCHAR(63) GENERATED ALWAYS AS (`Names` ->> ""$[0]"") VIRTUAL");
+                entity.Property(m => m.Name)
+                    .ValueGeneratedOnAddOrUpdate()
+                    .HasColumnType(@"VARCHAR(63) GENERATED ALWAYS AS (`Names` ->> ""$[0]"") VIRTUAL");
 
-				entity.Property(m => m.Email)
-					.ValueGeneratedOnAddOrUpdate()
-					.HasColumnType(@"VARCHAR(63) GENERATED ALWAYS AS (`ContactInfo` ->> ""$.Email"") VIRTUAL");
+                entity.Property(m => m.Email)
+                    .ValueGeneratedOnAddOrUpdate()
+                    .HasColumnType(@"VARCHAR(63) GENERATED ALWAYS AS (`ContactInfo` ->> ""$.Email"") VIRTUAL");
 
-				entity.Property(m => m.Address)
-					.ValueGeneratedOnAddOrUpdate()
-					.HasColumnType(@"VARCHAR(63) GENERATED ALWAYS AS (CONCAT_WS(', ',	`ContactInfo` ->> ""$.Address"", `ContactInfo` ->> ""$.City"", `ContactInfo` ->> ""$.State"", `ContactInfo` ->> ""$.Zip"")) STORED");
-			});
-		}
-	}
+                entity.Property(m => m.Address)
+                    .ValueGeneratedOnAddOrUpdate()
+                    .HasColumnType(@"VARCHAR(63) GENERATED ALWAYS AS (CONCAT_WS(', ',	`ContactInfo` ->> ""$.Address"", `ContactInfo` ->> ""$.City"", `ContactInfo` ->> ""$.State"", `ContactInfo` ->> ""$.Zip"")) STORED");
+            });
+        }
+    }
 
-	public class GeneratedContact
-	{
-	    public Guid Id { get; set; }
+    public class GeneratedContact
+    {
+        public Guid Id { get; set; }
 
-		public string Name { get; set; }
+        public string Name { get; set; }
 
-		public string Email { get; set; }
+        public string Email { get; set; }
 
-		public string Address { get; set; }
+        public string Address { get; set; }
 
-		public JsonObject<List<string>> Names { get; set; }
+        public JsonObject<List<string>> Names { get; set; }
 
-		public JsonObject<Dictionary<string, string>> ContactInfo { get; set; }
-	}
+        public JsonObject<Dictionary<string, string>> ContactInfo { get; set; }
+    }
 
-	public class GeneratedTime
-	{
-		public int Id { get; set; }
+    public class GeneratedTime
+    {
+        public int Id { get; set; }
 
-		public string Name { get; set; }
+        public string Name { get; set; }
 
-		[Column(TypeName = "DATETIME")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime CreatedDateTime { get; set; }
+        [Column(TypeName = "DATETIME")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedDateTime { get; set; }
 
-		[Column(TypeName = "DATETIME")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime UpdatedDateTime { get; set; }
+        [Column(TypeName = "DATETIME")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedDateTime { get; set; }
 
-		[Column(TypeName = "DATETIME(3)")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime CreatedDateTime3 { get; set; }
+        [Column(TypeName = "DATETIME(3)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedDateTime3 { get; set; }
 
-		[Column(TypeName = "DATETIME(3)")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime UpdatedDateTime3 { get; set; }
+        [Column(TypeName = "DATETIME(3)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedDateTime3 { get; set; }
 
-		// DATETIME(6) is the default
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime CreatedDateTime6 { get; set; }
+        // DATETIME(6) is the default
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedDateTime6 { get; set; }
 
-		// DATETIME(6) is the default
-		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime UpdatedDateTime6 { get; set; }
+        // DATETIME(6) is the default
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedDateTime6 { get; set; }
 
-		[Column(TypeName = "TIMESTAMP")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime CreatedTimestamp { get; set; }
+        [Column(TypeName = "TIMESTAMP")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedTimestamp { get; set; }
 
-		[Column(TypeName = "TIMESTAMP")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime UpdatedTimetamp { get; set; }
+        [Column(TypeName = "TIMESTAMP")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedTimetamp { get; set; }
 
-		[Column(TypeName = "TIMESTAMP(3)")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime CreatedTimestamp3 { get; set; }
+        [Column(TypeName = "TIMESTAMP(3)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedTimestamp3 { get; set; }
 
-		[Column(TypeName = "TIMESTAMP(3)")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime UpdatedTimetamp3 { get; set; }
+        [Column(TypeName = "TIMESTAMP(3)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedTimetamp3 { get; set; }
 
-		[Column(TypeName = "TIMESTAMP(6)")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime CreatedTimestamp6 { get; set; }
+        [Column(TypeName = "TIMESTAMP(6)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedTimestamp6 { get; set; }
 
-		[Column(TypeName = "TIMESTAMP(6)")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime UpdatedTimetamp6 { get; set; }
-	}
+        [Column(TypeName = "TIMESTAMP(6)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedTimetamp6 { get; set; }
+    }
 
     public interface IGeneratedConcurrencyModel
     {
@@ -111,16 +114,16 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Models
         int Gen { get; set; }
     }
 
-	public class GeneratedConcurrencyCheck : IGeneratedConcurrencyModel
-	{
-		public int Id { get; set; }
+    public class GeneratedConcurrencyCheck : IGeneratedConcurrencyModel
+    {
+        public int Id { get; set; }
 
-		public int Gen { get; set; }
+        public int Gen { get; set; }
 
-	    [ConcurrencyCheck]
-	    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-		public DateTime Updated { get; set; }
-	}
+        [ConcurrencyCheck]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Updated { get; set; }
+    }
 
     public class GeneratedRowVersion : IGeneratedConcurrencyModel
     {
