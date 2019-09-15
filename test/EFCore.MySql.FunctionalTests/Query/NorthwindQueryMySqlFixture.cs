1b25eded223de68c6239b19c05fd20c9c6399c36
Laurents Meyer <laucomm@gmail.com>
2019-09-15 15:05:05 +0200

    Update Northwind to 3.0.

--- test/EFCore.MySql.FunctionalTests/Query/NorthwindQueryMySqlFixture.cs
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindQueryMySqlFixture.cs
@@ -35,6 +35,12 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
                         {
                             CustomerID = o.CustomerID
                         }));
+
+            modelBuilder
+                .Entity<CustomerView>()
+                .HasNoKey()
+                .ToQuery(() => northwindContext.CustomerQueries
+                    .FromSqlInterpolated($"SELECT `c`.`Address`, `c`.`City`, `c`.`CompanyName`, `c`.`ContactName`, `c`.`ContactTitle`, `c`.`Country`, `c`.`Fax`, `c`.`Phone`, `c`.`PostalCode`, `c`.`Region` FROM `Customers` AS `c`"));
         }
 
         protected override void Seed(NorthwindContext context)
