c4b56a87536bd07592a67750c68a125f3c7229d6
Laurents Meyer <laucomm@gmail.com>
2019-09-15 14:43:28 +0200

    Upgrade MySqlRegexIsMatchTranslator to 3.0.

--- src/EFCore.MySql/Query/ExpressionTranslators/Internal/MySqlRegexIsMatchTranslator.cs
+++ src/EFCore.MySql/Query/ExpressionTranslators/Internal/MySqlRegexIsMatchTranslator.cs
@@ -8,18 +8,25 @@ using Pomelo.EntityFrameworkCore.MySql.Query.Expressions.Internal;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System.Collections.Generic;
+using Pomelo.EntityFrameworkCore.MySql.Query.Internal;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Query.ExpressionTranslators.Internal
 {
-
     public class MySqlRegexIsMatchTranslator : IMethodCallTranslator
     {
         private static readonly MethodInfo _methodInfo
             = typeof(Regex).GetRuntimeMethod(nameof(Regex.IsMatch), new[] { typeof(string), typeof(string) });
 
+        private readonly MySqlSqlExpressionFactory _sqlExpressionFactory;
+
+        public MySqlRegexIsMatchTranslator(ISqlExpressionFactory sqlExpressionFactory)
+        {
+            _sqlExpressionFactory = (MySqlSqlExpressionFactory)sqlExpressionFactory;
+        }
+
         public SqlExpression Translate(SqlExpression instance, MethodInfo method, IReadOnlyList<SqlExpression> arguments)
             => _methodInfo.Equals(method)
-                ? new RegexpExpression(
+                ? _sqlExpressionFactory.Regexp(
                     arguments[0],
                     arguments[1])
                 : null;
