8bc73cb82c37e4f4610ba05f03f46c4fc9501b41
Laurents Meyer <laucomm@gmail.com>
2019-09-16 12:11:17 +0200

    Remove SQL Server specific validator implementations.

--- src/EFCore.MySql/Internal/MySqlModelValidator.cs
+++ src/EFCore.MySql/Internal/MySqlModelValidator.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.DependencyInjection;
-using Pomelo.EntityFrameworkCore.MySql.Extensions;
-using Pomelo.EntityFrameworkCore.MySql.Metadata.Internal;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Internal
 {
@@ -51,83 +44,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
         public override void Validate(IModel model, IDiagnosticsLogger<DbLoggerCategory.Model.Validation> logger)
         {
             base.Validate(model, logger);
-
-            ValidateNonKeyValueGeneration(model, logger);
-        }
-
-        /// <summary>
-        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-        ///     any release. You should only use it directly in your code with extreme caution and knowing that
-        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-        /// </summary>
-        protected virtual void ValidateNonKeyValueGeneration([NotNull] IModel model, [NotNull] IDiagnosticsLogger<DbLoggerCategory.Model.Validation> logger)
-        {
-            foreach (var property in model.GetEntityTypes()
-                .SelectMany(t => t.GetDeclaredProperties())
-                .Where(
-                    p => ((IConventionProperty)p).GetValueGenerationStrategyConfigurationSource() != null
-                         && !p.IsKey()
-                         && p.ValueGenerated != ValueGenerated.Never
-                         && (!(p.FindAnnotation(MySqlAnnotationNames.ValueGenerationStrategy) is ConventionAnnotation strategy)
-                             || !ConfigurationSource.Convention.Overrides(strategy.GetConfigurationSource()))))
-            {
-                throw new InvalidOperationException(
-                    MySqlStrings.NonKeyValueGeneration(property.Name, property.DeclaringEntityType.DisplayName()));
-            }
-        }
-
-        /// <summary>
-        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-        ///     any release. You should only use it directly in your code with extreme caution and knowing that
-        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-        /// </summary>
-        protected override void ValidateSharedColumnsCompatibility(
-            IReadOnlyList<IEntityType> mappedTypes, string tableName, IDiagnosticsLogger<DbLoggerCategory.Model.Validation> logger)
-        {
-            base.ValidateSharedColumnsCompatibility(mappedTypes, tableName, logger);
-
-            var identityColumns = new List<IProperty>();
-            var propertyMappings = new Dictionary<string, IProperty>();
-
-            foreach (var property in mappedTypes.SelectMany(et => et.GetDeclaredProperties()))
-            {
-                var columnName = property.GetColumnName();
-                if (propertyMappings.TryGetValue(columnName, out var duplicateProperty))
-                {
-                    var propertyStrategy = property.GetValueGenerationStrategy();
-                    var duplicatePropertyStrategy = duplicateProperty.GetValueGenerationStrategy();
-                    if (propertyStrategy != duplicatePropertyStrategy
-                        && (propertyStrategy == MySqlValueGenerationStrategy.IdentityColumn
-                            || duplicatePropertyStrategy == MySqlValueGenerationStrategy.IdentityColumn))
-                    {
-                        throw new InvalidOperationException(
-                            MySqlStrings.DuplicateColumnNameValueGenerationStrategyMismatch(
-                                duplicateProperty.DeclaringEntityType.DisplayName(),
-                                duplicateProperty.Name,
-                                property.DeclaringEntityType.DisplayName(),
-                                property.Name,
-                                columnName,
-                                tableName));
-                    }
-                }
-                else
-                {
-                    propertyMappings[columnName] = property;
-                    if (property.GetValueGenerationStrategy() == MySqlValueGenerationStrategy.IdentityColumn)
-                    {
-                        identityColumns.Add(property);
-                    }
-                }
-            }
-
-            if (identityColumns.Count > 1)
-            {
-                var sb = new StringBuilder()
-                    .AppendJoin(identityColumns.Select(p => "'" + p.DeclaringEntityType.DisplayName() + "." + p.Name + "'"));
-                throw new InvalidOperationException(MySqlStrings.MultipleIdentityColumns(sb, tableName));
-            }
         }
     }
 }
