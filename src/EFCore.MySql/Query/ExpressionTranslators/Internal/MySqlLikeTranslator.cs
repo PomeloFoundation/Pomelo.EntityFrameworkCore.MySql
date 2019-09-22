a5255dcdc3f2b743cec4cd995da6c4e39f716958
James Yeung <shunjiey@hotmail.com>
2019-09-22 09:04:30 +0800

    Add a generic Like method to support more types in `LIKE` expressions (#806)
    * add a generic Like method to support more type in `LIKE` expression
    
    * fix comment
    
    * use `IConvertible` to convert generic value to string

---
+++ src/EFCore.MySql/Query/ExpressionTranslators/Internal/MySqlLikeTranslator.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Pomelo Foundation. All rights reserved.
+// Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
+using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators;
+using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal;
+
+namespace Pomelo.EntityFrameworkCore.MySql.Query.ExpressionTranslators.Internal
+{
+    /// <summary>
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class MySqlLikeTranslator : LikeTranslator, IMethodCallTranslator
+    {
+        private static readonly Type[] _supportedTypes = {
+            typeof(int),
+            typeof(long),
+            typeof(DateTime),
+            typeof(Guid),
+            typeof(bool),
+            typeof(byte),
+            typeof(byte[]),
+            typeof(double),
+            typeof(DateTimeOffset),
+            typeof(char),
+            typeof(short),
+            typeof(float),
+            typeof(decimal),
+            typeof(TimeSpan),
+            typeof(uint),
+            typeof(ushort),
+            typeof(ulong),
+            typeof(sbyte),
+            typeof(int?),
+            typeof(long?),
+            typeof(DateTime?),
+            typeof(Guid?),
+            typeof(bool?),
+            typeof(byte?),
+            typeof(double?),
+            typeof(DateTimeOffset?),
+            typeof(char?),
+            typeof(short?),
+            typeof(float?),
+            typeof(decimal?),
+            typeof(TimeSpan?),
+            typeof(uint?),
+            typeof(ushort?),
+            typeof(ulong?),
+            typeof(sbyte?),
+        };
+
+        private static readonly MethodInfo[] _methodInfos
+            = typeof(MySqlDbFunctionsExtensions).GetRuntimeMethods()
+                .Where(method => method.Name == nameof(MySqlDbFunctionsExtensions.Like)
+                                 && method.IsGenericMethod)
+                .SelectMany(method => _supportedTypes.Select(type => method.MakeGenericMethod(type))).ToArray();
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public override Expression Translate(MethodCallExpression methodCallExpression)
+        {
+            return _methodInfos.Any(method => Equals(methodCallExpression.Method, method))
+                       ? methodCallExpression.Method.GetParameters().Length == 3
+                           ? new LikeExpression(methodCallExpression.Arguments[1], methodCallExpression.Arguments[2])
+                           : new LikeExpression(methodCallExpression.Arguments[1], methodCallExpression.Arguments[2], methodCallExpression.Arguments[3])
+                       : base.Translate(methodCallExpression);
+        }
+    }
+}
--- test/EFCore.MySql.FunctionalTests/Query/DbFunctionsMySqlTest.cs
+++ test/EFCore.MySql.FunctionalTests/Query/DbFunctionsMySqlTest.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.EntityFrameworkCore.TestUtilities.Xunit;
 using Xunit;
 using Xunit.Abstractions;
+using Pomelo.EntityFrameworkCore.MySql.Query;
 
 namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
 {
@@ -128,6 +131,131 @@ WHERE TIMESTAMPDIFF(SECOND, `c`.`OrderDate`, CURRENT_TIMESTAMP()) = 0");
 FROM `Orders` AS `c`
 WHERE TIMESTAMPDIFF(MICROSECOND, CURRENT_TIMESTAMP(), DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 1.0 second)) = 0");
             }
+        }
+
+        public override void Like_literal()
+        {
+            base.Like_literal();
+
+            AssertSql(
+                @"SELECT COUNT(*)
+FROM `Customers` AS `c`
+WHERE `c`.`ContactName` LIKE '%M%'");
+        }
+
+        public override void Like_identity()
+        {
+            base.Like_identity();
+
+            AssertSql(
+                @"SELECT COUNT(*)
+FROM `Customers` AS `c`
+WHERE `c`.`ContactName` LIKE `c`.`ContactName`");
+        }
+
+        public override void Like_literal_with_escape()
+        {
+            base.Like_literal_with_escape();
+
+            AssertSql(
+                @"SELECT COUNT(*)
+FROM `Customers` AS `c`
+WHERE `c`.`ContactName` LIKE '!%' ESCAPE '!'");
+        }
+
+        [ConditionalFact]
+        public virtual void Like_Int_literal()
+        {
+            using (var context = CreateContext())
+            {
+                var count = context.Orders.Count(c => EF.Functions.Like(c.OrderID, "%M%"));
+
+                Assert.Equal(0, count);
+
+                AssertSql(
+                    @"SELECT COUNT(*)
+FROM `Orders` AS `c`
+WHERE `c`.`OrderID` LIKE '%M%'");
+            }
+        }
+
+        [ConditionalFact]
+        public virtual void Like_DateTime_literal()
+        {
+            using (var context = CreateContext())
+            {
+                var count = context.Orders.Count(c => EF.Functions.Like(c.OrderDate, "%M%"));
+
+                Assert.Equal(0, count);
+
+                AssertSql(
+                    @"SELECT COUNT(*)
+FROM `Orders` AS `c`
+WHERE `c`.`OrderDate` LIKE '%M%'");
+            }
+        }
+
+        [ConditionalFact]
+        public virtual void Like_Uint_literal()
+        {
+            using (var context = CreateContext())
+            {
+                var count = context.Orders.Count(c => EF.Functions.Like(c.EmployeeID, "%M%"));
+
+                Assert.Equal(0, count);
+
+                AssertSql(
+                    @"SELECT COUNT(*)
+FROM `Orders` AS `c`
+WHERE `c`.`EmployeeID` LIKE '%M%'");
+            }
+        }
+
+        [ConditionalFact]
+        public virtual void Like_Short_literal()
+        {
+            using (var context = CreateContext())
+            {
+                var count = context.OrderDetails.Count(c => EF.Functions.Like(c.Quantity, "%M%"));
+
+                Assert.Equal(0, count);
+
+                AssertSql(
+                    @"SELECT COUNT(*)
+FROM `Order Details` AS `c`
+WHERE `c`.`Quantity` LIKE '%M%'");
+            }
+        }
+
+        [ConditionalFact]
+        public virtual void Like_Int_literal_with_escape()
+        {
+            using (var context = CreateContext())
+            {
+                var count = context.Orders.Count(c => EF.Functions.Like(c.OrderID, "!%", "!"));
+
+                Assert.Equal(0, count);
+
+                AssertSql(
+                    @"SELECT COUNT(*)
+FROM `Orders` AS `c`
+WHERE `c`.`OrderID` LIKE '!%' ESCAPE '!'");
+            }
+        }
+
+        [ConditionalFact]
+        public virtual void Like_Client_InvariantCulture()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("zh-CN");
+            DateTime? exampleDate = new DateTime(2019, 8, 1, 18, 32, 6);
+
+            Assert.True(EF.Functions.Like(exampleDate, "08/01/2019%"));
+
+            double? d = 12.34D;
+            Assert.True(EF.Functions.Like(d, "12.3%"));
+
+            byte[] b = { 0x30, 0x31 };
+            Assert.True(EF.Functions.Like(b, "30%"));
         }
 
         private void AssertSql(params string[] expected) => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
