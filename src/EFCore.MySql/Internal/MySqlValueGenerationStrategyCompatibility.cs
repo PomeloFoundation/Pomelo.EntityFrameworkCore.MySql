b5b335098796ae300b1fc775b03eb0b048d3c955
Laurents Meyer <laucomm@gmail.com>
2019-10-12 15:32:07 +0200

    Fix ValueGeneration for `datetime` and `timestamp` columns in migrations (#870)
    * Adding a `datetime` or `timestamp` column with the `IdentityColumn` or `ComputedColumn` option set and no explicit default value specified, should leave the default value in the column operation null, so that CURRENT_TIMESTAMP will be used.
    
    * The class `MigrationsModelDiffer` is an internal class. We need to override it though, to fix #801.

---
+++ src/EFCore.MySql/Internal/MySqlValueGenerationStrategyCompatibility.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Pomelo.EntityFrameworkCore.MySql.Metadata.Internal;
+
+namespace Pomelo.EntityFrameworkCore.MySql.Internal
+{
+    public static class MySqlValueGenerationStrategyCompatibility
+    {
+        public static MySqlValueGenerationStrategy? GetValueGenerationStrategy(IAnnotation[] annotations)
+        {
+            var valueGenerationStrategy = annotations.FirstOrDefault(a => a.Name == MySqlAnnotationNames.ValueGenerationStrategy)?.Value as MySqlValueGenerationStrategy?;
+
+            if (!valueGenerationStrategy.HasValue ||
+                valueGenerationStrategy == MySqlValueGenerationStrategy.None)
+            {
+                var generatedOnAddAnnotation = annotations.FirstOrDefault(a => a.Name == MySqlAnnotationNames.LegacyValueGeneratedOnAdd)?.Value;
+                if (generatedOnAddAnnotation != null && (bool)generatedOnAddAnnotation)
+                {
+                    valueGenerationStrategy = MySqlValueGenerationStrategy.IdentityColumn;
+                }
+
+                var generatedOnAddOrUpdateAnnotation = annotations.FirstOrDefault(a => a.Name == MySqlAnnotationNames.LegacyValueGeneratedOnAddOrUpdate)?.Value;
+                if (generatedOnAddOrUpdateAnnotation != null && (bool)generatedOnAddOrUpdateAnnotation)
+                {
+                    valueGenerationStrategy = MySqlValueGenerationStrategy.ComputedColumn;
+                }
+            }
+
+            return valueGenerationStrategy;
+        }
+    }
+}
