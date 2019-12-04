7387b6f612e4b56ab1311be2d096213a0e8e7a2c
Laurents Meyer <laucomm@gmail.com>
2019-12-04 06:47:42 +0100

    Implement custom xUnit orderers. (#980)
    (cherry picked from commit fba81ee2aa46e9340460cdb730918c07934855c1)

--- test/EFCore.MySql.FunctionalTests/AssemblyInfo.cs
+++ test/EFCore.MySql.FunctionalTests/AssemblyInfo.cs
@@ -1,7 +1,14 @@
 using Xunit;
-//Optional
-// [assembly: CollectionBehavior(DisableTestParallelization = true)]
-//Optional
-[assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
-//Optional
-[assembly: TestCollectionOrderer("Xunit.Extensions.Ordering.CollectionOrderer", "Xunit.Extensions.Ordering")]
+
+//
+// Optional: Control the test execution order.
+//           This can be helpful for diffing etc.
+//
+
+#if FIXED_TEST_ORDER
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+[assembly: TestCollectionOrderer("Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities.Xunit.MySqlTestCollectionOrderer", "Pomelo.EntityFrameworkCore.MySql.FunctionalTests")]
+[assembly: TestCaseOrderer("Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities.Xunit.MySqlTestCaseOrderer", "Pomelo.EntityFrameworkCore.MySql.FunctionalTests")]
+
+#endif
