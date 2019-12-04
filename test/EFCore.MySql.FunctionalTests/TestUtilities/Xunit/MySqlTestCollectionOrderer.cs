fba81ee2aa46e9340460cdb730918c07934855c1
Laurents Meyer <laucomm@gmail.com>
2019-12-04 06:47:42 +0100

    Implement custom xUnit orderers. (#980)

---
+++ test/EFCore.MySql.FunctionalTests/TestUtilities/Xunit/MySqlTestCollectionOrderer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities.Xunit
+{
+    public class MySqlTestCollectionOrderer : ITestCollectionOrderer
+    {
+        public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+            => testCollections.OrderBy(c => c.DisplayName);
+    }
+}
