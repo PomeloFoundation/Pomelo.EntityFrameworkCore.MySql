094e678c294554c5dc91e936eddbcc07f9890157
Sjors Gielen <s.gielen@nl.ccv.eu>
2019-12-09 11:19:23 +0100

    Address naming issues.

--- test/EFCore.MySql.Tests/ValueGeneration/Internal/MysqlSequentialGuidValueGeneratorTest.cs
+++ test/EFCore.MySql.Tests/ValueGeneration/Internal/MysqlSequentialGuidValueGeneratorTest.cs
@@ -7,12 +7,11 @@ using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 using Pomelo.EntityFrameworkCore.MySql.ValueGeneration.Internal;
 using Xunit;
 
-// ReSharper disable InconsistentNaming
-namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.ValueGeneration.Internal
+namespace Pomelo.EntityFrameworkCore.MySql.Tests.ValueGeneration.Internal
 {
     public class MysqlSequentialGuidValueGeneratorTest
     {
-        private MySqlSequentialGuidValueGenerator getGenerator(bool mysqlGuidFormatIsLittleEndianBinary16) {
+        private MySqlSequentialGuidValueGenerator GetGenerator(bool mysqlGuidFormatIsLittleEndianBinary16) {
             var connectionSettings = new Mock<MySqlConnectionSettings>();
             connectionSettings.SetupGet(x => x.GuidFormat).Returns(mysqlGuidFormatIsLittleEndianBinary16 ? MySqlGuidFormat.LittleEndianBinary16 : MySqlGuidFormat.None);
 
@@ -23,27 +22,27 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.ValueGeneration.Inter
         }
 
         [Fact]
-        public void NextReturnsNewGuid()
+        public void Next_returns_new_Guid()
         {
-            var guid = getGenerator(false).Next();
+            var guid = GetGenerator(false).Next();
             Assert.NotEqual(Guid.Empty, guid);
         }
 
         [Fact]
-        public void NextReturnsNewGuidLEB16()
+        public void Next_returns_new_Guid_LittleEndianBinary16()
         {
-            var guid = getGenerator(true).Next();
+            var guid = GetGenerator(true).Next();
             Assert.NotEqual(Guid.Empty, guid);
         }
 
         [Fact]
-        public void NextReturnsLooselySortedGuid()
+        public void Next_returns_loosely_sorted_Guid()
         {
             // GUIDs are loosely sorted on time. When one GUID is generated at
             // least one tick (100 ns) after the last, it is sorted higher than
             // the previous one. This property gives us a performance benefit
             // on sorting in the database.
-            var generator = getGenerator(false);
+            var generator = GetGenerator(false);
 
             var timeNow = DateTimeOffset.UtcNow;
             var lastGuid = generator.Next(timeNow);
@@ -56,13 +55,13 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.ValueGeneration.Inter
         }
 
         [Fact]
-        public void NextReturnsLooselySortedGuidLEB16()
+        public void Next_returns_loosely_sorted_Guid_LittleEndianBinary16()
         {
             // GUIDs are loosely sorted on time. When one GUID is generated at
             // least one tick (100 ns) after the last, it is sorted higher than
             // the previous one. This property gives us a performance benefit
             // on sorting in the database.
-            var generator = getGenerator(true);
+            var generator = GetGenerator(true);
 
             var timeNow = DateTimeOffset.UtcNow;
             var lastGuid = generator.Next(timeNow);
@@ -75,9 +74,9 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.ValueGeneration.Inter
         }
 
         [Fact]
-        public void NextReturnsValidRFC4122RandomGuid()
+        public void Next_returns_valid_Rfc4122_random_Guid()
         {
-            var generator = getGenerator(false);
+            var generator = GetGenerator(false);
 
             // Since the values are based on randomness and time, perform this test
             // multiple times to increase the chance of finding incorrect values.
@@ -94,9 +93,9 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.ValueGeneration.Inter
         }
 
         [Fact]
-        public void NextReturnsValidRFC4122RandomGuidLEB16()
+        public void Next_returns_valid_Rfc4122_random_Guid_LittleEndianBinary16()
         {
-            var generator = getGenerator(true);
+            var generator = GetGenerator(true);
 
             // Since the values are based on randomness and time, perform this test
             // multiple times to increase the chance of finding incorrect values.
@@ -113,18 +112,18 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.ValueGeneration.Inter
         }
 
         [Fact]
-        public void NextReturnsUniqueGuid()
+        public void Next_returns_unique_Guid()
         {
-            var generator = getGenerator(false);
+            var generator = GetGenerator(false);
 
             var guids = Enumerable.Range(0, 100).Select(_ => generator.Next()).ToArray();
             Assert.Equal(guids.Distinct().Count(), guids.Length);
         }
 
         [Fact]
-        public void NextReturnsUniqueGuidLEB16()
+        public void Next_returns_unique_Guid_LittleEndianBinary16()
         {
-            var generator = getGenerator(true);
+            var generator = GetGenerator(true);
 
             var guids = Enumerable.Range(0, 100).Select(_ => generator.Next()).ToArray();
             Assert.Equal(guids.Distinct().Count(), guids.Length);
