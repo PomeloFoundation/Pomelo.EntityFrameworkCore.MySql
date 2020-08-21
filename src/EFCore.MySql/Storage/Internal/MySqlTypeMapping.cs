76851bed9d710e44800bb073a1f10755a8054785
Laurents Meyer <laucomm@gmail.com>
2020-08-21 21:20:03 +0200

    Add support for the YEAR store type. (#1143)

---
+++ src/EFCore.MySql/Storage/Internal/MySqlTypeMapping.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MySql.Data.MySqlClient;
+
+namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
+{
+    // TODO: Use as base class for all type mappings.
+    /// <summary>
+    /// The base class for mapping MySql-specific types. It configures parameters with the
+    /// <see cref="MySqlDbType"/> provider-specific type enum.
+    /// </summary>
+    public abstract class MySqlTypeMapping : RelationalTypeMapping
+    {
+        /// <summary>
+        /// The database type used by MySql.
+        /// </summary>
+        public virtual MySqlDbType MySqlDbType { get; }
+
+        // ReSharper disable once PublicConstructorInAbstractClass
+        /// <summary>
+        /// Constructs an instance of the <see cref="MySqlTypeMapping"/> class.
+        /// </summary>
+        /// <param name="storeType">The database type to map.</param>
+        /// <param name="clrType">The CLR type to map.</param>
+        /// <param name="mySqlDbType">The database type used by MySql.</param>
+        public MySqlTypeMapping(
+            [NotNull] string storeType,
+            [NotNull] Type clrType,
+            MySqlDbType mySqlDbType,
+            DbType? dbType = null,
+            bool unicode = false,
+            int? size = null,
+            ValueConverter valueConverter = null)
+            : base(
+                new RelationalTypeMappingParameters(
+                    new CoreTypeMappingParameters(clrType, valueConverter), storeType, StoreTypePostfix.None, dbType, unicode, size))
+            => MySqlDbType = mySqlDbType;
+
+        /// <summary>
+        /// Constructs an instance of the <see cref="MySqlTypeMapping"/> class.
+        /// </summary>
+        /// <param name="parameters">The parameters for this mapping.</param>
+        /// <param name="mySqlDbType">The database type of the range subtype.</param>
+        protected MySqlTypeMapping(RelationalTypeMappingParameters parameters, MySqlDbType mySqlDbType)
+            : base(parameters)
+            => MySqlDbType = mySqlDbType;
+
+        protected override void ConfigureParameter(DbParameter parameter)
+        {
+            var mySqlParameter = parameter as MySqlParameter;
+            if (mySqlParameter == null)
+            {
+                throw new ArgumentException($"MySql-specific type mapping {GetType()} being used with non-MySql parameter type {parameter.GetType().Name}");
+            }
+
+            base.ConfigureParameter(parameter);
+
+            mySqlParameter.MySqlDbType = MySqlDbType;
+        }
+    }
+}
---
+++ src/EFCore.MySql/Storage/Internal/MySqlYearTypeMapping.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Storage;
+using MySql.Data.MySqlClient;
+
+namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
+{
+    public class MySqlYearTypeMapping : MySqlTypeMapping
+    {
+        public MySqlYearTypeMapping([NotNull] string storeType)
+            : base(
+                storeType,
+                typeof(short),
+                MySqlDbType.Year,
+                System.Data.DbType.Int16)
+        {
+        }
+
+        protected MySqlYearTypeMapping(RelationalTypeMappingParameters parameters, MySqlDbType mySqlDbType)
+            : base(parameters, mySqlDbType)
+        {
+        }
+
+        protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
+            => new MySqlYearTypeMapping(parameters, MySqlDbType);
+    }
+}
