PomeloFoundation/Pomelo.EntityFrameworkCore.MySql

commit cec4a4ec8131580836eabdb26770a4e927d6c355

Improve type mapping source and support all `datetime`, `timestamp` and `time` precisions between 0 and 6 (#1066)
* Remove debugging output from yml.

* Improve `datetime`, `timestamp` and `time` handling, to support all precision between 0 and 6.
Fix minor issues and inaccuracies in type mapping source.
Add missing `MySqlDateTimeType.Timestamp` value.
Refactor `MySqlTypeMappingSource` extensively.

src/EFCore.MySql/Internal/MySqlDefaultDataTypeMappings.cs
@@ -103,7 +103,8 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
         Default = 0,
         DateTime = 1,
         DateTime6 = 2,
-        Timestamp6 = 3
+        Timestamp6 = 3,
+        Timestamp = 4,
     }
 
     public enum MySqlTimeSpanType

src/EFCore.MySql/Internal/MySqlOptions.cs
@@ -133,7 +133,8 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
         {
             defaultDataTypeMappings ??= DefaultDataTypeMappings;
 
-            if (connectionSettings.TreatTinyAsBoolean)
+            if (connectionSettings.TreatTinyAsBoolean ||
+                defaultDataTypeMappings.ClrBoolean == MySqlBooleanType.Default)
             {
                 defaultDataTypeMappings = defaultDataTypeMappings.WithClrBoolean(MySqlBooleanType.TinyInt1);
             }
@@ -141,6 +142,30 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
                      defaultDataTypeMappings.ClrBoolean != MySqlBooleanType.None)
             {
                 defaultDataTypeMappings = defaultDataTypeMappings.WithClrBoolean(MySqlBooleanType.Bit1);
+            }
+
+            if (defaultDataTypeMappings.ClrDateTime == MySqlDateTimeType.Default)
+            {
+                defaultDataTypeMappings = defaultDataTypeMappings.WithClrDateTime(
+                    ServerVersion.SupportsDateTime6
+                        ? MySqlDateTimeType.DateTime6
+                        : MySqlDateTimeType.DateTime);
+            }
+
+            if (defaultDataTypeMappings.ClrDateTimeOffset == MySqlDateTimeType.Default)
+            {
+                defaultDataTypeMappings = defaultDataTypeMappings.WithClrDateTimeOffset(
+                    ServerVersion.SupportsDateTime6
+                        ? MySqlDateTimeType.DateTime6
+                        : MySqlDateTimeType.DateTime);
+            }
+
+            if (defaultDataTypeMappings.ClrTimeSpan == MySqlTimeSpanType.Default)
+            {
+                defaultDataTypeMappings = defaultDataTypeMappings.WithClrTimeSpan(
+                    ServerVersion.SupportsDateTime6
+                        ? MySqlTimeSpanType.Time6
+                        : MySqlTimeSpanType.Time);
             }
 
             return defaultDataTypeMappings;

src/EFCore.MySql/Storage/Internal/MySqlDateTimeOffsetTypeMapping.cs
@@ -2,12 +2,9 @@
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
 using System;
-using System.Data;
 using System.Data.Common;
-using System.Globalization;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
 {
@@ -19,9 +16,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
     /// </summary>
     public class MySqlDateTimeOffsetTypeMapping : DateTimeOffsetTypeMapping
     {
-        private const string DateTimeOffsetFormatConst6 = @"yyyy-MM-dd HH\:mm\:ss.ffffff";
-        private const string DateTimeOffsetFormatConst = @"yyyy-MM-dd HH\:mm\:ss";
-
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
         ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
@@ -35,7 +29,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
                 new RelationalTypeMappingParameters(
                     new CoreTypeMappingParameters(typeof(DateTimeOffset)),
                     storeType,
-                    precision == null ? StoreTypePostfix.None : StoreTypePostfix.Precision,
+                    StoreTypePostfix.Precision,
                     System.Data.DbType.DateTimeOffset,
                     precision: precision))
         {
@@ -58,25 +52,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
             => new MySqlDateTimeOffsetTypeMapping(parameters);
 
-        public override string GenerateProviderValueSqlLiteral([CanBeNull] object value)
-            => value == null
-                ? "NULL"
-                : GenerateNonNullSqlLiteral(
-                    value is DateTimeOffset dateTimeOffset
-                        ? dateTimeOffset.UtcDateTime
-                        : value);
-
-        /// <summary>
-        ///     Gets the string format to be used to generate SQL literals of this type.
-        /// </summary>
-        protected override string SqlLiteralFormatString
-            => "'{0:" + GetFormatString() + "}'";
-
-        public string GetFormatString() =>
-            Parameters.Precision == null
-                ? DateTimeOffsetFormatConst
-                : DateTimeOffsetFormatConst6;
-
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
         ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
@@ -92,6 +67,32 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
             {
                 parameter.Size = Size.Value;
             }
+        }
+
+        public override string GenerateProviderValueSqlLiteral([CanBeNull] object value)
+            => value == null
+                ? "NULL"
+                : GenerateNonNullSqlLiteral(
+                    value is DateTimeOffset dateTimeOffset
+                        ? dateTimeOffset.UtcDateTime
+                        : value);
+
+        /// <summary>
+        ///     Gets the string format to be used to generate SQL literals of this type.
+        /// </summary>
+        protected override string SqlLiteralFormatString
+            => $"'{{0:{GetFormatString()}}}'";
+
+        public virtual string GetFormatString()
+            => GetDateTimeOffsetFormatString(Parameters.Precision);
+
+        public static string GetDateTimeOffsetFormatString(int? precision)
+        {
+            var validPrecision = Math.Min(Math.Max(precision.GetValueOrDefault(), 0), 6);
+            var precisionFormat = validPrecision > 0
+                ? @"." + new string('F', validPrecision)
+                : null;
+            return @"yyyy-MM-dd HH\:mm\:ss" + precisionFormat;
         }
     }
 }

src/EFCore.MySql/Storage/Internal/MySqlDateTimeTypeMapping.cs
@@ -2,8 +2,7 @@
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
 using System;
-using System.Data;
-using System.Data.Common;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -19,9 +18,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
     /// </summary>
     public class MySqlDateTimeTypeMapping : DateTimeTypeMapping
     {
-        private const string DateTimeFormatConst6 = @"'{0:yyyy-MM-dd HH\:mm\:ss.ffffff}'";
-        private const string DateTimeFormatConst = @"'{0:yyyy-MM-dd HH\:mm\:ss}'";
-
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
         ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
@@ -30,15 +26,15 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         /// </summary>
         public MySqlDateTimeTypeMapping(
             [NotNull] string storeType,
-            Type clrType,
-            ValueConverter converter = null,
-            ValueComparer comparer = null,
-            int? precision = null)
+            int? precision = null,
+            Type clrType = null,
+            [CanBeNull] ValueConverter converter = null,
+            [CanBeNull] ValueComparer comparer = null)
             : this(
                 new RelationalTypeMappingParameters(
-                    new CoreTypeMappingParameters(clrType, converter, comparer),
+                    new CoreTypeMappingParameters(clrType ?? typeof(DateTime), converter, comparer),
                     storeType,
-                    precision == null ? StoreTypePostfix.None : StoreTypePostfix.Precision,
+                    StoreTypePostfix.Precision,
                     System.Data.DbType.DateTime,
                     precision: precision))
         {
@@ -65,6 +61,18 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         ///     Gets the string format to be used to generate SQL literals of this type.
         /// </summary>
         protected override string SqlLiteralFormatString
-            => Parameters.Precision == null ? DateTimeFormatConst : DateTimeFormatConst6;
+            => $"'{{0:{GetFormatString()}}}'";
+
+        public virtual string GetFormatString()
+            => GetDateTimeFormatString(Parameters.Precision);
+
+        public static string GetDateTimeFormatString(int? precision)
+        {
+            var validPrecision = Math.Min(Math.Max(precision.GetValueOrDefault(), 0), 6);
+            var precisionFormat = validPrecision > 0
+                ? "." + new string('F', validPrecision)
+                : null;
+            return @"yyyy-MM-dd HH\:mm\:ss" + precisionFormat;
+        }
     }
 }

src/EFCore.MySql/Storage/Internal/MySqlDateTypeMapping.cs
@@ -5,7 +5,6 @@ using System;
 using System.Data;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
 {
@@ -26,10 +25,8 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         ///     any release. You should only use it directly in your code with extreme caution and knowing that
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
-        public MySqlDateTypeMapping(
-            [NotNull] string storeType,
-            DbType? dbType = null)
-            : base(storeType, dbType)
+        public MySqlDateTypeMapping([NotNull] string storeType)
+            : base(storeType, System.Data.DbType.Date)
         {
         }
 

src/EFCore.MySql/Storage/Internal/MySqlJsonTypeMapping.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -32,15 +31,14 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         /// </summary>
         public MySqlJsonTypeMapping(
             Type clrType,
-            string storeType = null,
-            bool? unicode = null)
+            string storeType = null)
             : this(
                 new RelationalTypeMappingParameters(
                     new CoreTypeMappingParameters(clrType, GetConverter(clrType), GetComprarer(clrType)),
                     storeType ?? "json",
                     StoreTypePostfix.None,
                     System.Data.DbType.String,
-                    unicode ?? false))
+                    true))
         {
         }
 
@@ -72,6 +70,9 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         /// <returns> The newly created mapping. </returns>
         protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
             => new MySqlJsonTypeMapping(parameters);
+
+        public virtual RelationalTypeMapping Clone(Type clrType)
+            => new MySqlJsonTypeMapping(clrType, StoreType);
 
         /// <summary>
         ///     Generates the escaped SQL representation of a literal value.

src/EFCore.MySql/Storage/Internal/MySqlStringTypeMapping.cs
@@ -30,7 +30,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
             [NotNull] string storeType,
             DbType? dbType,
             IMySqlOptions options,
-            bool unicode = false,
+            bool unicode = true,
             int? size = null,
             bool fixedLength = false)
             : this(
@@ -96,7 +96,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
             {
                 length = null;
             }
-            
+
             parameter.Value = value;
             parameter.Size = value == null || value == DBNull.Value || length != null && length <= _maxSpecificSize
                 ? _maxSpecificSize

src/EFCore.MySql/Storage/Internal/MySqlTimeSpanTypeMapping.cs
@@ -2,9 +2,10 @@
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
 using System;
+using System.Globalization;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore.Utilities;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
 {
@@ -27,7 +28,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
                 new RelationalTypeMappingParameters(
                     new CoreTypeMappingParameters(typeof(TimeSpan)),
                     storeType,
-                    precision == null ? StoreTypePostfix.None : StoreTypePostfix.Precision,
+                    StoreTypePostfix.Precision,
                     System.Data.DbType.Time,
                     precision: precision))
         {
@@ -51,8 +52,37 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
             => new MySqlTimeSpanTypeMapping(parameters);
 
         /// <summary>
+        ///     Generates the SQL representation of a non-null literal value.
+        /// </summary>
+        /// <param name="value">The literal value.</param>
+        /// <returns>
+        ///     The generated string.
+        /// </returns>
+        protected override string GenerateNonNullSqlLiteral([NotNull] object value)
+        {
+            // Custom TimeSpan formats do not handle the fraction point character as gracefully as System.DateTime does.
+            var literal = base.GenerateNonNullSqlLiteral(value);
+            return literal.EndsWith(".0")
+                ? "'" + literal.Substring(0, literal.Length - 2) + "'"
+                : "'" + literal + "'";
+        }
+
+        /// <summary>
         ///     Gets the string format to be used to generate SQL literals of this type.
         /// </summary>
-        protected override string SqlLiteralFormatString => "'{0}'";
+        protected override string SqlLiteralFormatString
+            => $"{{0:{GetFormatString()}}}";
+
+        public virtual string GetFormatString()
+            => GetTimeSpanFormatString(Parameters.Precision);
+
+        public static string GetTimeSpanFormatString(int? precision)
+        {
+            var validPrecision = Math.Min(Math.Max(precision.GetValueOrDefault(), 0), 6);
+            var precisionFormat = validPrecision > 0
+                ? @"\.f" + new string('F', validPrecision - 1)
+                : null;
+            return @"hh\:mm\:ss" + precisionFormat;
+        }
     }
 }

commit 9f2cff32ec0bc7f51b03ee09f42e246e87e6c0e0

Fix regression bug introduced in cec4a4e. Add tests and upgrade information.

src/EFCore.MySql/Internal/MySqlDefaultDataTypeMappings.cs
@@ -92,7 +92,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
 
     public enum MySqlBooleanType
     {
-        None = -1,
+        None = -1, // TODO: Remove in EF Core 5; see MySqlTypeMappingTest.Bool_with_MySqlBooleanType_None_maps_to_null()
         Default = 0,
         TinyInt1 = 1,
         Bit1 = 2

src/EFCore.MySql/Internal/MySqlOptions.cs
@@ -20,7 +20,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
         {
             ConnectionSettings = new MySqlConnectionSettings();
             ServerVersion = new ServerVersion(null);
-            CharSetBehavior = CharSetBehavior.AppendToAllColumns;
+            CharSetBehavior = CharSetBehavior.AppendToAllColumns; // TODO: Change to `NeverAppend` for EF Core 5.
 
             // We do not use the MySQL versions's default, but explicitly use `utf8mb4`
             // if not changed by the user.
@@ -157,15 +157,14 @@ namespace Pomelo.EntityFrameworkCore.MySql.Internal
         {
             defaultDataTypeMappings ??= DefaultDataTypeMappings;
 
-            if (connectionSettings.TreatTinyAsBoolean ||
+            // Explicitly set MySqlDefaultDataTypeMappings values take precedence over connection string options.
+            if (connectionSettings.TreatTinyAsBoolean.HasValue &&
                 defaultDataTypeMappings.ClrBoolean == MySqlBooleanType.Default)
             {
-                defaultDataTypeMappings = defaultDataTypeMappings.WithClrBoolean(MySqlBooleanType.TinyInt1);
-            }
-            else if (defaultDataTypeMappings.ClrBoolean != MySqlBooleanType.Bit1 &&
-                     defaultDataTypeMappings.ClrBoolean != MySqlBooleanType.None)
-            {
-                defaultDataTypeMappings = defaultDataTypeMappings.WithClrBoolean(MySqlBooleanType.Bit1);
+                defaultDataTypeMappings = defaultDataTypeMappings.WithClrBoolean(
+                    connectionSettings.TreatTinyAsBoolean.Value
+                        ? MySqlBooleanType.TinyInt1
+                        : MySqlBooleanType.Bit1);
             }
 
             if (defaultDataTypeMappings.ClrDateTime == MySqlDateTimeType.Default)

src/EFCore.MySql/Storage/Internal/MySqlConnectionSettings.cs
@@ -1,7 +1,9 @@
 // Copyright (c) Pomelo Foundation. All rights reserved.
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
+using System;
 using System.Data.Common;
+using System.Linq;
 using MySql.Data.MySqlClient;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
@@ -32,11 +34,17 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
                 GuidFormat = csb.GuidFormat;
             }
 
-            TreatTinyAsBoolean = csb.TreatTinyAsBoolean;
+            // It would be nice to have access to a public and currently non-existing
+            // MySqlConnectionStringOption.TreatTinyAsBoolean.HasValue() method, so we can safely find out, whether
+            // TreatTinyAsBoolean has been explicitly set or not.
+            var treatTinyAsBooleanKeys = new[] {"Treat Tiny As Boolean", "TreatTinyAsBoolean"};
+            TreatTinyAsBoolean = treatTinyAsBooleanKeys.Any(k => csb.ContainsKey(k))
+                ? (bool?)csb.TreatTinyAsBoolean
+                : null;
         }
 
         public virtual MySqlGuidFormat GuidFormat { get; }
-        public virtual bool TreatTinyAsBoolean { get; }
+        public virtual bool? TreatTinyAsBoolean { get; }
 
         protected bool Equals(MySqlConnectionSettings other)
         {
@@ -65,11 +73,8 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         }
 
         public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((int)GuidFormat * 397) ^ TreatTinyAsBoolean.GetHashCode();
-            }
-        }
+            => HashCode.Combine(
+                GuidFormat,
+                TreatTinyAsBoolean);
     }
 }

test/EFCore.MySql.FunctionalTests/Storage/MySqlTypeMappingTest.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design.Internal;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pomelo.EntityFrameworkCore.MySql.Internal;
-using Pomelo.EntityFrameworkCore.MySql.Json.Microsoft.Storage.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 using Xunit;
 
@@ -379,24 +375,88 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Storage
 
         #endregion Ranges
 */
+        [Fact]
+        public void Bool_with_MySqlBooleanType_None_maps_to_null()
+        {
+            // TODO: EF Core 5
+            // If MySqlBooleanType.None is used, the intention was to not map System.Boolean at all, but EF Core
+            // successfully substitutes System.Boolean with System.Int32 and then maps it to INT.
+            // So for EF Core 5, we should remove the MySqlBooleanType.None option altogether.
+            var options = GetOptions(b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.None)));
+            var mapping = GetMapping(typeof(bool), options);
+
+            Assert.Equal("int", mapping.StoreType);
+        }
+
+        [Fact]
+        public void Bool_with_MySqlBooleanType_Default_maps_to_tinyint1()
+        {
+            var options = GetOptions(b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.Default)));
+            var mapping = GetMapping(typeof(bool), options);
+
+            Assert.Equal("tinyint(1)", mapping.StoreType);
+        }
+
+        [Fact]
+        public void Bool_with_MySqlBooleanType_TinyInt1_maps_to_tinyint1()
+        {
+            var options = GetOptions(b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.TinyInt1)));
+            var mapping = GetMapping(typeof(bool), options);
+
+            Assert.Equal("tinyint(1)", mapping.StoreType);
+        }
+
+        [Fact]
+        public void Bool_with_MySqlBooleanType_Bit1_maps_to_bit1()
+        {
+            var options = GetOptions(b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.Bit1)));
+            var mapping = GetMapping(typeof(bool), options);
+
+            Assert.Equal("bit(1)", mapping.StoreType);
+        }
+
+        [Fact]
+        public void Bit1_with_MySqlBooleanType_Default_maps_to_UInt64()
+        {
+            var options = GetOptions(b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.Default)));
+            var mapping = GetMapping("bit(1)", options);
+
+            Assert.Equal(typeof(ulong), mapping.ClrType);
+        }
+
         #region Support
 
-        private static readonly MySqlTypeMappingSource Mapper = new MySqlTypeMappingSource(
-            new TypeMappingSourceDependencies(
-                new ValueConverterSelector(new ValueConverterSelectorDependencies()),
-                Array.Empty<ITypeMappingSourcePlugin>()),
-            new RelationalTypeMappingSourceDependencies(
-                Array.Empty<IRelationalTypeMappingSourcePlugin>()),
-            new MySqlOptions()
-        );
+        private static MySqlTypeMappingSource GetMapper(MySqlOptions options = null)
+            => new MySqlTypeMappingSource(
+                new TypeMappingSourceDependencies(
+                    new ValueConverterSelector(new ValueConverterSelectorDependencies()),
+                    Array.Empty<ITypeMappingSourcePlugin>()),
+                new RelationalTypeMappingSourceDependencies(
+                    Array.Empty<IRelationalTypeMappingSourcePlugin>()),
+                options ?? new MySqlOptions());
 
-        private static RelationalTypeMapping GetMapping(string storeType) => Mapper.FindMapping(storeType);
+        private static RelationalTypeMapping GetMapping(string storeType, MySqlOptions options = null)
+            => GetMapper(options).FindMapping(storeType);
 
-        private static RelationalTypeMapping GetMapping(Type clrType) => (RelationalTypeMapping)Mapper.FindMapping(clrType);
+        private static RelationalTypeMapping GetMapping(Type clrType, MySqlOptions options = null)
+            => (RelationalTypeMapping)GetMapper(options).FindMapping(clrType);
 
-        private static readonly CSharpHelper CsHelper = new CSharpHelper(Mapper);
+        private static CSharpHelper GetCsHelper(MySqlOptions options = null)
+            => new CSharpHelper(GetMapper(options));
 
-        private static string CodeLiteral(object value) => CsHelper.UnknownLiteral(value);
+        private static string CodeLiteral(object value, MySqlOptions options = null)
+            => GetCsHelper(options).UnknownLiteral(value);
+
+        private static MySqlOptions GetOptions(Action<MySqlDbContextOptionsBuilder> builder)
+        {
+            var mySqlOptions = new MySqlOptions();
+            mySqlOptions.Initialize(
+                new DbContextOptionsBuilder()
+                    .UseMySql("Server=foo", builder)
+                    .Options);
+
+            return mySqlOptions;
+        }
 
         #endregion Support
     }

test/EFCore.MySql.Tests/MySqlDbContextOptionsExtensionsTest.cs
@@ -31,5 +31,74 @@ namespace Pomelo.EntityFrameworkCore.MySql
             Assert.StartsWith("Server=second;", mySqlOptionsExtension.ConnectionString, StringComparison.OrdinalIgnoreCase);
             Assert.Equal(MySqlBooleanType.TinyInt1, mySqlOptionsExtension.DefaultDataTypeMappings.ClrBoolean);
         }
+
+        [Fact]
+        public void TreatTinyAsBoolean_true()
+        {
+            var builder = new DbContextOptionsBuilder();
+
+            builder.UseMySql("TreatTinyAsBoolean=True");
+
+            var mySqlOptions = new MySqlOptions();
+            mySqlOptions.Initialize(builder.Options);
+
+            Assert.Equal(MySqlBooleanType.TinyInt1, mySqlOptions.DefaultDataTypeMappings.ClrBoolean);
+        }
+
+        [Fact]
+        public void TreatTinyAsBoolean_false()
+        {
+            var builder = new DbContextOptionsBuilder();
+
+            builder.UseMySql("TreatTinyAsBoolean=False");
+
+            var mySqlOptions = new MySqlOptions();
+            mySqlOptions.Initialize(builder.Options);
+
+            Assert.Equal(MySqlBooleanType.Bit1, mySqlOptions.DefaultDataTypeMappings.ClrBoolean);
+        }
+
+        [Fact]
+        public void TreatTinyAsBoolean_unspecified()
+        {
+            var builder = new DbContextOptionsBuilder();
+
+            builder.UseMySql("Server=foo");
+
+            var mySqlOptions = new MySqlOptions();
+            mySqlOptions.Initialize(builder.Options);
+
+            Assert.Equal(MySqlBooleanType.Default, mySqlOptions.DefaultDataTypeMappings.ClrBoolean);
+        }
+
+        [Fact]
+        public void Explicit_DefaultDataTypeMappings_take_precedence_over_TreatTinyAsBoolean_true()
+        {
+            var builder = new DbContextOptionsBuilder();
+
+            builder.UseMySql(
+                "TreatTinyAsBoolean=True",
+                b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.Bit1)));
+
+            var mySqlOptions = new MySqlOptions();
+            mySqlOptions.Initialize(builder.Options);
+
+            Assert.Equal(MySqlBooleanType.Bit1, mySqlOptions.DefaultDataTypeMappings.ClrBoolean);
+        }
+
+        [Fact]
+        public void Explicit_DefaultDataTypeMappings_take_precedence_over_TreatTinyAsBoolean_false()
+        {
+            var builder = new DbContextOptionsBuilder();
+
+            builder.UseMySql(
+                "TreatTinyAsBoolean=False",
+                b => b.DefaultDataTypeMappings(m => m.WithClrBoolean(MySqlBooleanType.TinyInt1)));
+
+            var mySqlOptions = new MySqlOptions();
+            mySqlOptions.Initialize(builder.Options);
+
+            Assert.Equal(MySqlBooleanType.TinyInt1, mySqlOptions.DefaultDataTypeMappings.ClrBoolean);
+        }
     }
 }