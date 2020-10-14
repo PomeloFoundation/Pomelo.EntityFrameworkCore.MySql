2927740b2b359f2281f0791099499c3ab460b312
Laurents Meyer <laucomm@gmail.com>
2020-10-14 07:43:09 +0200

    Fix String.Contains() translation with non-constant pattern.

--- src/EFCore.MySql/Query/ExpressionTranslators/Internal/MySqlStringComparisonMethodTranslator.cs
+++ src/EFCore.MySql/Query/ExpressionTranslators/Internal/MySqlStringComparisonMethodTranslator.cs
@@ -435,14 +435,18 @@ namespace Pomelo.EntityFrameworkCore.MySql.Query.ExpressionTranslators.Internal
                 return _sqlExpressionFactory.Like(target, _sqlExpressionFactory.Constant(null, stringTypeMapping));
             }
 
-            // LOCATE('foo', 'barfoobar') > 0
+            // 'foo' = '' OR LOCATE('foo', 'barfoobar') > 0
             // Using an empty pattern `LOCATE('', 'barfoobar')` returns 1.
-            return _sqlExpressionFactory.GreaterThan(
-                _sqlExpressionFactory.NullableFunction(
-                    "LOCATE",
-                    new[] {patternTransform(pattern), targetTransform(target)},
-                    typeof(int)),
-                _sqlExpressionFactory.Constant(0));
+            return _sqlExpressionFactory.OrElse(
+                _sqlExpressionFactory.Equal(
+                    pattern,
+                    _sqlExpressionFactory.Constant(string.Empty, stringTypeMapping)),
+                _sqlExpressionFactory.GreaterThan(
+                    _sqlExpressionFactory.NullableFunction(
+                        "LOCATE",
+                        new[] {patternTransform(pattern), targetTransform(target)},
+                        typeof(int)),
+                        _sqlExpressionFactory.Constant(0)));
         }
 
         public SqlExpression MakeIndexOfExpression(
--- test/EFCore.MySql.FunctionalTests/Query/FunkyDataQueryMySqlTest.cs
+++ test/EFCore.MySql.FunctionalTests/Query/FunkyDataQueryMySqlTest.cs
@@ -1,10 +1,8 @@
-﻿using System.Threading.Tasks;
-using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+﻿using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Xunit;
 
 namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
 {
@@ -13,110 +11,15 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
         public FunkyDataQueryMySqlTest(FunkyDataQueryMySqlFixture fixture)
             : base(fixture)
         {
+            ClearLog();
+            //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
         }
 
-        [ConditionalTheory]
-        [MemberData(nameof(IsAsyncData))]
-        public override async Task String_contains_on_argument_with_wildcard_column(bool async)
-        {
-            await base.String_contains_on_argument_with_wildcard_column(async);
+        protected override void ClearLog()
+            => Fixture.TestSqlLoggerFactory.Clear();
 
-            // See https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/issues/996#issuecomment-607844082
-            //
-            // return AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Select(c => c.FirstName)
-            //         .SelectMany(c => ss.Set<FunkyCustomer>().Select(c2 => c2.LastName), (fn, ln) => new { fn, ln })
-            //         .Where(r => r.fn.Contains(r.ln)),
-            //     ss => ss.Set<FunkyCustomer>().Select(c => c.FirstName)
-            //         .SelectMany(c => ss.Set<FunkyCustomer>().Select(c2 => c2.LastName), (fn, ln) => new { fn, ln })
-            //         .Where(r => MaybeScalar(r.fn, () => MaybeScalar<bool>(r.ln, () => r.fn.Contains(r.ln))) == true),
-            //     elementSorter: e => (e.fn, e.ln),
-            //     elementAsserter: (e, a) =>
-            //     {
-            //         Assert.Equal(e.fn, a.fn);
-            //         Assert.Equal(e.ln, a.ln);
-            //     });
-        }
-
-        [ConditionalTheory]
-        [MemberData(nameof(IsAsyncData))]
-        public override async Task String_contains_on_argument_with_wildcard_column_negated(bool async)
-        {
-            await base.String_contains_on_argument_with_wildcard_column_negated(async);
-
-            // See https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/issues/996#issuecomment-607844082
-            //
-            // return AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Select(c => c.FirstName)
-            //         .SelectMany(c => ss.Set<FunkyCustomer>().Select(c2 => c2.LastName), (fn, ln) => new { fn, ln })
-            //         .Where(r => !r.fn.Contains(r.ln)),
-            //     ss => ss.Set<FunkyCustomer>().Select(c => c.FirstName)
-            //         .SelectMany(c => ss.Set<FunkyCustomer>().Select(c2 => c2.LastName), (fn, ln) => new { fn, ln })
-            //         .Where(r => !MaybeScalar(r.fn, () => MaybeScalar<bool>(r.ln, () => r.fn.Contains(r.ln))) == true));
-        }
-
-        [ConditionalTheory]
-        [MemberData(nameof(IsAsyncData))]
-        public override async Task String_contains_on_argument_with_wildcard_parameter(bool async)
-        {
-            await base.String_contains_on_argument_with_wildcard_parameter(async);
-
-            // See https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/issues/996#issuecomment-607844082
-            //
-            // var prm1 = "%B";
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => c.FirstName.Contains(prm1)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => MaybeScalar<bool>(c.FirstName, () => c.FirstName.Contains(prm1)) == true)
-            //         .Select(c => c.FirstName));
-            //
-            // var prm2 = "a_";
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => c.FirstName.Contains(prm2)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => MaybeScalar<bool>(c.FirstName, () => c.FirstName.Contains(prm2)) == true)
-            //         .Select(c => c.FirstName));
-            //
-            // var prm3 = (string)null;
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => c.FirstName.Contains(prm3)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => false).Select(c => c.FirstName));
-            //
-            // var prm4 = "";
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => c.FirstName.Contains(prm4)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => MaybeScalar<bool>(c.FirstName, () => c.FirstName.Contains(prm4)) == true).Select(c => c.FirstName));
-            //
-            // var prm5 = "_Ba_";
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => c.FirstName.Contains(prm5)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => MaybeScalar<bool>(c.FirstName, () => c.FirstName.Contains(prm5)) == true)
-            //         .Select(c => c.FirstName));
-            //
-            // var prm6 = "%B%a%r";
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => !c.FirstName.Contains(prm6)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => !MaybeScalar<bool>(c.FirstName, () => c.FirstName.Contains(prm6)) == true)
-            //         .Select(c => c.FirstName));
-            //
-            // var prm7 = "";
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => !c.FirstName.Contains(prm7)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => false).Select(c => c.FirstName));
-            //
-            // var prm8 = (string)null;
-            // await AssertQuery(
-            //     async,
-            //     ss => ss.Set<FunkyCustomer>().Where(c => !c.FirstName.Contains(prm8)).Select(c => c.FirstName),
-            //     ss => ss.Set<FunkyCustomer>().Where(c => false).Select(c => c.FirstName));
-        }
+        private void AssertSql(params string[] expected)
+            => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
 
         public class FunkyDataQueryMySqlFixture : FunkyDataQueryFixtureBase
         {
