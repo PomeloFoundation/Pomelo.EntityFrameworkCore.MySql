885b03cc563fb08c6a0ffcd21a2ff3bbd692939a
Laurents Meyer <laucomm@gmail.com>
2020-10-29 16:17:12 +0100

    Implement NorthwindIncludeNoTrackingQueryMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindIncludeNoTrackingQueryMySqlTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class NorthwindIncludeNoTrackingQueryMySqlTest : NorthwindIncludeNoTrackingQueryTestBase<NorthwindQueryMySqlFixture<NoopModelCustomizer>>
+    {
+        // ReSharper disable once UnusedParameter.Local
+        public NorthwindIncludeNoTrackingQueryMySqlTest(NorthwindQueryMySqlFixture<NoopModelCustomizer> fixture)
+            : base(fixture)
+            => Fixture.TestSqlLoggerFactory.Clear();
+
+        public override async Task Include_collection_with_last_no_orderby(bool async)
+            => Assert.Equal(
+                RelationalStrings.LastUsedWithoutOrderBy(nameof(Enumerable.Last)),
+                (await Assert.ThrowsAsync<InvalidOperationException>(
+                    () => base.Include_collection_with_last_no_orderby(async))).Message);
+    }
+}
