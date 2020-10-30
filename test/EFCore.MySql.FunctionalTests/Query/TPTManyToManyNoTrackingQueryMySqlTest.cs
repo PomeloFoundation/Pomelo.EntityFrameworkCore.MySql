2435e0d55e147641ed8f492672eae800176330f2
Laurents Meyer <laucomm@gmail.com>
2020-10-30 21:55:25 +0100

    Implement TPTManyToManyNoTrackingQueryMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/TPTManyToManyNoTrackingQueryMySqlTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Xunit.Abstractions;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class TPTManyToManyNoTrackingQueryMySqlTest : TPTManyToManyNoTrackingQueryRelationalTestBase<TPTManyToManyQueryMySqlFixture>
+    {
+        public TPTManyToManyNoTrackingQueryMySqlTest(TPTManyToManyQueryMySqlFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            Fixture.TestSqlLoggerFactory.Clear();
+            //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+        }
+
+        // TODO:
+        // protected override bool CanExecuteQueryString => true;
+    }
+}
