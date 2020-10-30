9635b0c4f54c6863b72656da28cb0b89ec21ed9e
Laurents Meyer <laucomm@gmail.com>
2020-10-30 14:34:22 +0100

    Implement NorthwindStringIncludeQueryMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindStringIncludeQueryMySqlTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class NorthwindStringIncludeQueryMySqlTest : NorthwindStringIncludeQueryTestBase<NorthwindQueryMySqlFixture<NoopModelCustomizer>>
+    {
+        // ReSharper disable once UnusedParameter.Local
+        public NorthwindStringIncludeQueryMySqlTest(NorthwindQueryMySqlFixture<NoopModelCustomizer> fixture)
+            : base(fixture)
+            => Fixture.TestSqlLoggerFactory.Clear();
+
+        public override async Task Include_collection_with_last_no_orderby(bool async)
+            => Assert.Equal(
+                RelationalStrings.LastUsedWithoutOrderBy(nameof(Enumerable.Last)),
+                (await Assert.ThrowsAsync<InvalidOperationException>(
+                    () => base.Include_collection_with_last_no_orderby(async))).Message);
+    }
+}
