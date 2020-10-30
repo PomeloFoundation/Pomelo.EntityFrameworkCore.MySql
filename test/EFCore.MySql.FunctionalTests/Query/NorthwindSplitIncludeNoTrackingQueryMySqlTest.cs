9f8111a38dfdae72850e03187aba57a73f3cc36d
Laurents Meyer <laucomm@gmail.com>
2020-10-30 14:36:33 +0100

    Implement NorthwindSplitIncludeNoTrackingQueryMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindSplitIncludeNoTrackingQueryMySqlTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit.Abstractions;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class NorthwindSplitIncludeNoTrackingQueryMySqlTest : NorthwindSplitIncludeNoTrackingQueryTestBase<NorthwindQueryMySqlFixture<NoopModelCustomizer>>
+    {
+        public NorthwindSplitIncludeNoTrackingQueryMySqlTest(NorthwindQueryMySqlFixture<NoopModelCustomizer> fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            //TestSqlLoggerFactory.CaptureOutput(testOutputHelper);
+        }
+
+        public override Task Include_collection_with_last_no_orderby(bool async)
+            => AssertTranslationFailedWithDetails(
+                () => AssertLast(
+                    async,
+                    ss => ss.Set<Customer>().Include(c => c.Orders),
+                    entryCount: 8
+                ), RelationalStrings.MissingOrderingInSqlExpression);
+    }
+}
