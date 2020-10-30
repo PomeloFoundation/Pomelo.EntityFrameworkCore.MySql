1c86804f97432f8acdae5a291b7c18598799858b
Laurents Meyer <laucomm@gmail.com>
2020-10-30 12:49:26 +0100

    Implement NorthwindJoinQueryMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindJoinQueryMySqlTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit.Abstractions;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class NorthwindJoinQueryMySqlTest : NorthwindJoinQueryRelationalTestBase<NorthwindQueryMySqlFixture<NoopModelCustomizer>>
+    {
+        public NorthwindJoinQueryMySqlTest(NorthwindQueryMySqlFixture<NoopModelCustomizer> fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            ClearLog();
+            //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+        }
+
+        protected override void ClearLog()
+            => Fixture.TestSqlLoggerFactory.Clear();
+    }
+}
