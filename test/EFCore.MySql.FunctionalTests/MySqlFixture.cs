c664b6e4ff3c5bd295b8303e6bc933844972b001
Laurents Meyer <laucomm@gmail.com>
2020-10-30 14:15:02 +0100

    Implement TwoDatabasesMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/MySqlFixture.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests
+{
+    public class MySqlFixture : ServiceProviderFixtureBase
+    {
+        public TestSqlLoggerFactory TestSqlLoggerFactory
+            => (TestSqlLoggerFactory)ServiceProvider.GetRequiredService<ILoggerFactory>();
+
+        protected override ITestStoreFactory TestStoreFactory
+            => MySqlTestStoreFactory.Instance;
+    }
+}
