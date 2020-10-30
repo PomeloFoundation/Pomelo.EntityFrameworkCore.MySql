d491ef42695e10a8d5ee8828be4e2a6eda5bafd7
Laurents Meyer <laucomm@gmail.com>
2020-10-30 12:56:18 +0100

    Implement NorthwindKeylessEntitiesQueryMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindKeylessEntitiesQueryMySqlTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class NorthwindKeylessEntitiesQueryMySqlTest : NorthwindKeylessEntitiesQueryRelationalTestBase<NorthwindQueryMySqlFixture<NoopModelCustomizer>>
+    {
+        public NorthwindKeylessEntitiesQueryMySqlTest(NorthwindQueryMySqlFixture<NoopModelCustomizer> fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            Fixture.TestSqlLoggerFactory.Clear();
+            //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+        }
+
+        [ConditionalFact(Skip = "https://github.com/dotnet/efcore/issues/21627")]
+        public override void KeylessEntity_with_nav_defining_query()
+            => base.KeylessEntity_with_nav_defining_query();
+    }
+}
