399fa8ce0711ad9ce707268df4017bb0c09e833f
Laurents Meyer <laucomm@gmail.com>
2021-01-23 13:35:08 +0100

    Fix issue regarding compound keys that also contain database generated columns other than auto_increment columns.

--- test/EFCore.MySql.FunctionalTests/UpdatesMySqlFixture.cs
+++ test/EFCore.MySql.FunctionalTests/UpdatesMySqlFixture.cs
@@ -1,3 +1,4 @@
+using System;
 using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.TestUtilities;
@@ -7,5 +8,37 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests
     public class UpdatesMySqlFixture : UpdatesRelationalFixture
     {
         protected override ITestStoreFactory TestStoreFactory => MySqlTestStoreFactory.Instance;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+        {
+            base.OnModelCreating(modelBuilder, context);
+
+            Models.Issue1300.Setup(modelBuilder, context);
+        }
+
+        public static class Models
+        {
+            public static class Issue1300
+            {
+                public static void Setup(ModelBuilder modelBuilder, DbContext context)
+                {
+                    modelBuilder.Entity<Flavor>(
+                        entity =>
+                        {
+                            entity.HasKey(e => new {e.FlavorId, e.DiscoveryDate});
+                            entity.Property(e => e.FlavorId)
+                                .ValueGeneratedOnAdd();
+                            entity.Property(e => e.DiscoveryDate)
+                                .ValueGeneratedOnAdd();
+                        });
+                }
+
+                public class Flavor
+                {
+                    public int FlavorId { get; set; }
+                    public DateTime DiscoveryDate { get; set; }
+                }
+            }
+        }
     }
 }
---
+++ test/EFCore.MySql.FunctionalTests/test/EFCore.MySql.FunctionalTests/UpdatesMySqlTest.MySql.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests
+{
+    public partial class UpdatesMySqlTest
+    {
+        private void AssertSql(params string[] expected)
+            => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
+
+        [ConditionalFact]
+        public virtual void Select_where_without_default_value_after_insert()
+        {
+            ExecuteWithStrategyInTransaction(
+                context =>
+                {
+                    context.Set<UpdatesMySqlFixture.Models.Issue1300.Flavor>().Add(
+                        new UpdatesMySqlFixture.Models.Issue1300.Flavor());
+
+                    context.SaveChanges();
+
+                    AssertSql(
+                        @"INSERT INTO `Flavor` ()
+VALUES ();
+SELECT `DiscoveryDate`, `FlavorId`
+FROM `Flavor`
+WHERE ROW_COUNT() = 1 AND `FlavorId` = LAST_INSERT_ID();");
+                },
+                context =>
+                {
+                    var flavors = context.Set<UpdatesMySqlFixture.Models.Issue1300.Flavor>()
+                        .ToList();
+
+                    Assert.Single(flavors);
+                    Assert.Equal(1, flavors[0].FlavorId);
+                    Assert.True(DateTime.Now - flavors[0].DiscoveryDate < TimeSpan.FromDays(1));
+                });
+        }
+    }
+}
