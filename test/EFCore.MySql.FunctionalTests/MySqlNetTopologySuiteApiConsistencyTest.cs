84f08f61109759007be1eca63b96e963563edec1
Laurents Meyer <laucomm@gmail.com>
2021-04-30 14:37:40 +0200

    Implement MySqlNetTopologySuiteApiConsistencyTest.

---
+++ test/EFCore.MySql.FunctionalTests/MySqlNetTopologySuiteApiConsistencyTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests
+{
+    public class MySqlNetTopologySuiteApiConsistencyTest : ApiConsistencyTestBase<MySqlNetTopologySuiteApiConsistencyTest.MySqlNetTopologySuiteApiConsistencyFixture>
+    {
+        public MySqlNetTopologySuiteApiConsistencyTest(MySqlNetTopologySuiteApiConsistencyFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        protected override void AddServices(ServiceCollection serviceCollection)
+            => serviceCollection.AddEntityFrameworkMySqlNetTopologySuite();
+
+        protected override Assembly TargetAssembly
+            => typeof(MySqlNetTopologySuiteServiceCollectionExtensions).Assembly;
+
+        public class MySqlNetTopologySuiteApiConsistencyFixture : ApiConsistencyFixtureBase
+        {
+            public override HashSet<Type> FluentApiTypes { get; } = new()
+            {
+                typeof(MySqlNetTopologySuiteDbContextOptionsBuilderExtensions),
+                typeof(MySqlNetTopologySuiteServiceCollectionExtensions)
+            };
+        }
+    }
+}
