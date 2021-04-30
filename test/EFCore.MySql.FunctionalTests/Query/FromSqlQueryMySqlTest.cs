23658fc1925f76f341ff9a72ef17322d6df6df68
Laurents Meyer <laucomm@gmail.com>
2021-05-01 00:14:07 +0200

    Skip failing FromSqlQuery test cases, until https://github.com/dotnet/efcore/pull/24806 has been merged.

--- test/EFCore.MySql.FunctionalTests/Query/FromSqlQueryMySqlTest.cs
+++ test/EFCore.MySql.FunctionalTests/Query/FromSqlQueryMySqlTest.cs
@@ -1,7 +1,9 @@
 using System.Data.Common;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using MySqlConnector;
+using Xunit;
 
 namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
 {
@@ -18,5 +20,21 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
                 ParameterName = name,
                 Value = value
             };
+
+        [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/pull/24806")]
+        public override Task FromSqlRaw_in_subquery_with_dbParameter(bool async)
+            => base.FromSqlRaw_in_subquery_with_dbParameter(async);
+
+        [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/pull/24806")]
+        public override Task FromSqlRaw_in_subquery_with_positional_dbParameter_with_name(bool async)
+            => base.FromSqlRaw_in_subquery_with_positional_dbParameter_with_name(async);
+
+        [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/pull/24806")]
+        public override Task FromSqlRaw_in_subquery_with_positional_dbParameter_without_name(bool async)
+            => base.FromSqlRaw_in_subquery_with_positional_dbParameter_without_name(async);
+
+        [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/pull/24806")]
+        public override Task FromSqlRaw_with_dbParameter_mixed_in_subquery(bool async)
+            => base.FromSqlRaw_with_dbParameter_mixed_in_subquery(async);
     }
 }
