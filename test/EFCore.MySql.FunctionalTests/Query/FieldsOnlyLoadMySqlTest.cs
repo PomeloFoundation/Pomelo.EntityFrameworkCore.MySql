4a3ab8c7527409f37220a34a6cf56b6721b82e04
Laurents Meyer <laucomm@gmail.com>
2021-05-01 00:15:45 +0200

    Implement FieldsOnlyLoadMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/FieldsOnlyLoadMySqlTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class FieldsOnlyLoadMySqlTest : FieldsOnlyLoadTestBase<FieldsOnlyLoadMySqlTest.FieldsOnlyLoadMySqlFixture>
+    {
+        public FieldsOnlyLoadMySqlTest(FieldsOnlyLoadMySqlFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class FieldsOnlyLoadMySqlFixture : FieldsOnlyLoadFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => MySqlTestStoreFactory.Instance;
+        }
+    }
+}
