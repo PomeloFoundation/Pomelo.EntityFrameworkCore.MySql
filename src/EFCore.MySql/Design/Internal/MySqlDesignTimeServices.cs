6d4b367991247cfd7684be62b18c2c43bf657e9e
Laurents Meyer <laucomm@gmail.com>
2021-05-01 00:39:18 +0200

    Implement DesignTimeMySqlTest.

--- src/EFCore.MySql/Design/Internal/MySqlDesignTimeServices.cs
+++ src/EFCore.MySql/Design/Internal/MySqlDesignTimeServices.cs
@@ -1,17 +1,10 @@
 // Copyright (c) Pomelo Foundation. All rights reserved.
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
-using Pomelo.EntityFrameworkCore.MySql.Diagnostics.Internal;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
-using Pomelo.EntityFrameworkCore.MySql.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
-using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Scaffolding;
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
 {
@@ -19,14 +12,12 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
     {
         public virtual void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
         {
-            serviceCollection
-                .AddSingleton<LoggingDefinitions, MySqlLoggingDefinitions>()
-                .AddSingleton<IRelationalTypeMappingSource, MySqlTypeMappingSource>()
-                .AddSingleton<IDatabaseModelFactory, MySqlDatabaseModelFactory>()
-                .AddSingleton<IProviderConfigurationCodeGenerator, MySqlCodeGenerator>()
-                .AddSingleton<IAnnotationCodeGenerator, MySqlAnnotationCodeGenerator>();
-
-            serviceCollection.TryAddSingleton<IMySqlOptions, MySqlOptions>();
+            serviceCollection.AddEntityFrameworkMySql();
+            new EntityFrameworkRelationalDesignServicesBuilder(serviceCollection)
+                .TryAdd<IAnnotationCodeGenerator, MySqlAnnotationCodeGenerator>()
+                .TryAdd<IDatabaseModelFactory, MySqlDatabaseModelFactory>()
+                .TryAdd<IProviderConfigurationCodeGenerator, MySqlCodeGenerator>()
+                .TryAddCoreServices();
         }
     }
 }
---
+++ test/EFCore.MySql.FunctionalTests/DesignTimeMySqlTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Pomelo.EntityFrameworkCore.MySql.Design.Internal;
+using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests
+{
+    public class DesignTimeMySqlTest : DesignTimeTestBase<DesignTimeMySqlTest.DesignTimeMySqlFixture>
+    {
+        public DesignTimeMySqlTest(DesignTimeMySqlFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        protected override Assembly ProviderAssembly
+            => typeof(MySqlDesignTimeServices).Assembly;
+
+        public class DesignTimeMySqlFixture : DesignTimeFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => MySqlTestStoreFactory.Instance;
+        }
+    }
+}
