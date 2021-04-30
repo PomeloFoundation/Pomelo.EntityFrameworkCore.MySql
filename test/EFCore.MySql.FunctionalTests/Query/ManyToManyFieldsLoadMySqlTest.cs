01ecefa851ad6addd7b14b2fc6035d69001ffe2b
Laurents Meyer <laucomm@gmail.com>
2021-05-01 00:54:16 +0200

    Implement ManyToManyFieldsLoadMySqlTest.

---
+++ test/EFCore.MySql.FunctionalTests/Query/ManyToManyFieldsLoadMySqlTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestModels.ManyToManyFieldsModel;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class ManyToManyFieldsLoadMySqlTest : ManyToManyFieldsLoadTestBase<
+        ManyToManyFieldsLoadMySqlTest.ManyToManyFieldsLoadMySqlFixture>
+    {
+        public ManyToManyFieldsLoadMySqlTest(ManyToManyFieldsLoadMySqlFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class ManyToManyFieldsLoadMySqlFixture : ManyToManyFieldsLoadFixtureBase
+        {
+            public TestSqlLoggerFactory TestSqlLoggerFactory
+                => (TestSqlLoggerFactory)ListLoggerFactory;
+
+            protected override ITestStoreFactory TestStoreFactory
+                => MySqlTestStoreFactory.Instance;
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+            {
+                base.OnModelCreating(modelBuilder, context);
+
+                modelBuilder
+                    .Entity<JoinOneSelfPayload>()
+                    .Property(e => e.Payload)
+                    .ValueGeneratedOnAdd();
+
+                modelBuilder
+                    .SharedTypeEntity<Dictionary<string, object>>("JoinOneToThreePayloadFullShared")
+                    .IndexerProperty<string>("Payload")
+                    .HasDefaultValue("Generated");
+
+                modelBuilder
+                    .Entity<JoinOneToThreePayloadFull>()
+                    .Property(e => e.Payload)
+                    .HasDefaultValue("Generated");
+            }
+        }
+    }
+}
