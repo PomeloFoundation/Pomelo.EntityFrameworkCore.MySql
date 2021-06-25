65aa615d2c164daa2658d70c3310f53b083fb197
Laurents Meyer <laucomm@gmail.com>
2021-06-25 09:42:39 +0200

    Add new tests.

---
+++ test/EFCore.MySql.FunctionalTests/Query/ComplexNavigationsCollectionsSplitSharedTypeQueryMySqlTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Xunit.Abstractions;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class ComplexNavigationsCollectionsSplitSharedTypeQueryMySqlTest : ComplexNavigationsCollectionsSplitSharedQueryTypeRelationalTestBase<ComplexNavigationsSharedTypeQueryMySqlFixture>
+    {
+        public ComplexNavigationsCollectionsSplitSharedTypeQueryMySqlTest(
+            ComplexNavigationsSharedTypeQueryMySqlFixture fixture,
+            ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            Fixture.TestSqlLoggerFactory.Clear();
+            //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+        }
+    }
+}
---
+++ test/EFCore.MySql.FunctionalTests/Query/ComplexNavigationsSharedTypeQueryMySqlFixture.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
+{
+    public class ComplexNavigationsSharedTypeQueryMySqlFixture : ComplexNavigationsSharedTypeQueryRelationalFixtureBase
+    {
+        protected override ITestStoreFactory TestStoreFactory
+            => MySqlTestStoreFactory.Instance;
+    }
+}
