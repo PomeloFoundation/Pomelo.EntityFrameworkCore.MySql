9864de6b69154f874624c853beb1a6aa4a2e3fc9
Laurents Meyer <laucomm@gmail.com>
2021-10-07 03:37:21 +0200

    Clean-up xUnit file structure and obsolete code.

---
+++ test/EFCore.MySql.FunctionalTests/TestUtilities/Xunit/MySqlConditionalFactTestCase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.TestUtilities.Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities.Xunit
+{
+    /// <remarks>
+    /// We cannot inherit from ConditionalFactTestCase, because it's sealed.
+    /// </remarks>
+    public sealed class MySqlConditionalFactTestCase : XunitTestCase
+    {
+        [Obsolete("Called by the de-serializer; should only be called by deriving classes for de-serialization purposes")]
+        public MySqlConditionalFactTestCase()
+        {
+        }
+
+        public MySqlConditionalFactTestCase(
+            IMessageSink diagnosticMessageSink,
+            TestMethodDisplay defaultMethodDisplay,
+            TestMethodDisplayOptions defaultMethodDisplayOptions,
+            ITestMethod testMethod,
+            object[] testMethodArguments = null)
+            : base(diagnosticMessageSink, defaultMethodDisplay, defaultMethodDisplayOptions, testMethod, testMethodArguments)
+        {
+        }
+
+        public override async Task<RunSummary> RunAsync(
+            IMessageSink diagnosticMessageSink,
+            IMessageBus messageBus,
+            object[] constructorArguments,
+            ExceptionAggregator aggregator,
+            CancellationTokenSource cancellationTokenSource)
+            => await XunitTestCaseExtensions.TrySkipAsync(this, messageBus)
+                ? new RunSummary { Total = 1, Skipped = 1 }
+                : await new MySqlXunitTestCaseRunner(
+                    this,
+                    DisplayName,
+                    SkipReason,
+                    constructorArguments,
+                    TestMethodArguments,
+                    messageBus,
+                    aggregator,
+                    cancellationTokenSource).RunAsync();
+    }
+}
---
+++ test/EFCore.MySql.FunctionalTests/TestUtilities/Xunit/MySqlXunitTheoryTestCaseRunner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities.Xunit
+{
+    public class MySqlXunitTheoryTestCaseRunner : XunitTheoryTestCaseRunner
+    {
+        public MySqlXunitTheoryTestCaseRunner(
+            IXunitTestCase testCase,
+            string displayName,
+            string skipReason,
+            object[] constructorArguments,
+            IMessageSink diagnosticMessageSink,
+            IMessageBus messageBus,
+            ExceptionAggregator aggregator,
+            CancellationTokenSource cancellationTokenSource)
+            : base(
+                testCase,
+                displayName,
+                skipReason,
+                constructorArguments,
+                diagnosticMessageSink,
+                messageBus,
+                aggregator,
+                cancellationTokenSource)
+        {
+        }
+
+        protected override XunitTestRunner CreateTestRunner(
+            ITest test,
+            IMessageBus messageBus,
+            Type testClass,
+            object[] constructorArguments,
+            MethodInfo testMethod,
+            object[] testMethodArguments,
+            string skipReason,
+            IReadOnlyList<BeforeAfterTestAttribute> beforeAfterAttributes,
+            ExceptionAggregator aggregator,
+            CancellationTokenSource cancellationTokenSource)
+            => new MySqlXunitTestRunner(
+                test,
+                messageBus,
+                testClass,
+                constructorArguments,
+                testMethod,
+                testMethodArguments,
+                skipReason,
+                beforeAfterAttributes,
+                new ExceptionAggregator(aggregator),
+                cancellationTokenSource);
+    }
+}
