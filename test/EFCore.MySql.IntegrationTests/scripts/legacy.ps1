29e56d8fefbe930f06c23e141656c6a760d068bd
rodrigue10 <60077909+rodrigue10@users.noreply.github.com>
2021-10-13 13:04:29 -0500

    Update legacy.ps1
    revert to dotnet

--- test/EFCore.MySql.IntegrationTests/scripts/legacy.ps1
+++ test/EFCore.MySql.IntegrationTests/scripts/legacy.ps1
@@ -4,21 +4,21 @@ Push-Location (Join-Path (Split-Path $MyInvocation.MyCommand.Path) "../")
 
 try
 {
-  C:\Program Files\dotnet\dotnet.exe tool restore;
+  dotnet tool restore;
 
   $legacyVersions = Get-ChildItem 'LegacyMigrations' -Directory | % { $_.Name }
 
   foreach ($legacyVersion in $legacyVersions)
   {
       Remove-Item (Join-Path 'Migrations' '*.cs')
-      C:\'Program Files'\dotnet\dotnet.exe ef database drop -f
+      dotnet ef database drop -f
 
       Copy-Item (Join-Path 'LegacyMigrations' $legacyVersion '*.csbak') 'Migrations'
       Get-ChildItem (Join-Path 'Migrations' '*.csbak') | % { Rename-Item -Path $_.FullName -NewName ([System.IO.Path]::ChangeExtension($_.Name, ".cs")) }
 
-     C:\'Program Files'\dotnet\dotnet.exe ef migrations add current --verbose
-    C:\'Program Files'\dotnet\dotnet.exe ef database update --verbose
-     C:\'Program Files'\dotnet\dotnet.exe test
+     dotnet ef migrations add current --verbose
+    dotnet ef database update --verbose
+     dotnet test
       break;
   }
 }
