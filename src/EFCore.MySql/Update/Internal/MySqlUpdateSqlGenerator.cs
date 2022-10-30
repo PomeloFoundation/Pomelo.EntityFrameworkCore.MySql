6950018f0c7a417e911ceb12f2ed87c9b9bdcfc0
Laurents Meyer <laucomm@gmail.com>
2022-10-31 00:16:13 +0100

    Fix MySqlUpdateSqlGenerator and tests.

--- src/EFCore.MySql/Update/Internal/MySqlUpdateSqlGenerator.cs
+++ src/EFCore.MySql/Update/Internal/MySqlUpdateSqlGenerator.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT. See LICENSE in the project root for license information.
 
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -11,43 +10,39 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.EntityFrameworkCore.Utilities;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
 
 namespace Pomelo.EntityFrameworkCore.MySql.Update.Internal
 {
-    // TODO: Revamp
     public class MySqlUpdateSqlGenerator : UpdateAndSelectSqlGenerator, IMySqlUpdateSqlGenerator
     {
-        /// <summary>
-        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-        ///     any release. You should only use it directly in your code with extreme caution and knowing that
-        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-        /// </summary>
+        [NotNull] private readonly IMySqlOptions _options;
+
         public MySqlUpdateSqlGenerator(
-            [NotNull] UpdateSqlGeneratorDependencies dependencies)
+            [NotNull] UpdateSqlGeneratorDependencies dependencies,
+            [NotNull] IMySqlOptions options)
             : base(dependencies)
         {
+            _options = options;
         }
 
-        /// <summary>
-        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-        ///     any release. You should only use it directly in your code with extreme caution and knowing that
-        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-        /// </summary>
+        public override ResultSetMapping AppendInsertOperation(
+            StringBuilder commandStringBuilder,
+            IReadOnlyModificationCommand command,
+            int commandPosition,
+            out bool requiresTransaction)
+            => _options.ServerVersion.Supports.Returning ||
+               command.ColumnModifications.All(o => !o.IsRead)
+                ? AppendInsertReturningOperation(commandStringBuilder, command, commandPosition, out requiresTransaction)
+                : base.AppendInsertOperation(commandStringBuilder, command, commandPosition, out requiresTransaction);
+
         public virtual ResultSetMapping AppendBulkInsertOperation(
             StringBuilder commandStringBuilder,
             IReadOnlyList<IReadOnlyModificationCommand> modificationCommands,
             int commandPosition,
             out bool requiresTransaction)
         {
-            var table = StoreObjectIdentifier.Table(modificationCommands[0].TableName, modificationCommands[0].Schema);
-
-            if (modificationCommands.Count == 1
-                && modificationCommands[0].ColumnModifications.All(o =>
-                    !o.IsKey
-                    || !o.IsRead
-                    || o.Property?.GetValueGenerationStrategy(table) == MySqlValueGenerationStrategy.IdentityColumn))
+            if (modificationCommands.Count == 1)
             {
                 return AppendInsertOperation(commandStringBuilder, modificationCommands[0], commandPosition, out requiresTransaction);
             }
@@ -55,33 +50,9 @@ namespace Pomelo.EntityFrameworkCore.MySql.Update.Internal
             var readOperations = modificationCommands[0].ColumnModifications.Where(o => o.IsRead).ToList();
             var writeOperations = modificationCommands[0].ColumnModifications.Where(o => o.IsWrite).ToList();
 
-            var writableOperations = modificationCommands[0].ColumnModifications
-                .Where(o => o.Property?.GetValueGenerationStrategy(table) != MySqlValueGenerationStrategy.IdentityColumn
-                            && o.Property?.GetComputedColumnSql() is null)
-                .ToList();
-
-            var defaultValuesOnly = writeOperations.Count == 0;
-            if (defaultValuesOnly)
-            {
-                if (writableOperations.Count == 0
-                    || readOperations.Count == 0)
-                {
-                    requiresTransaction = modificationCommands.Count > 1;
-                    foreach (var modification in modificationCommands)
-                    {
-                        AppendInsertOperation(commandStringBuilder, modification, commandPosition, out var localRequiresTransaction);
-                        requiresTransaction = requiresTransaction || localRequiresTransaction;
-                    }
-
-                    return readOperations.Count == 0
-                        ? ResultSetMapping.NoResults
-                        : ResultSetMapping.LastInResultSet;
-                }
-            }
-
             if (readOperations.Count == 0)
             {
-                return AppendBulkInsertWithoutServerValues(commandStringBuilder, modificationCommands, writeOperations, out requiresTransaction);
+                return AppendInsertMultipleRowsInSingleStatementOperation(commandStringBuilder, modificationCommands, writeOperations, out requiresTransaction);
             }
 
             requiresTransaction = modificationCommands.Count > 1;
@@ -94,14 +65,12 @@ namespace Pomelo.EntityFrameworkCore.MySql.Update.Internal
             return ResultSetMapping.LastInResultSet;
         }
 
-        private ResultSetMapping AppendBulkInsertWithoutServerValues(
+        private ResultSetMapping AppendInsertMultipleRowsInSingleStatementOperation(
             StringBuilder commandStringBuilder,
             IReadOnlyList<IReadOnlyModificationCommand> modificationCommands,
             List<IColumnModification> writeOperations,
             out bool requiresTransaction)
         {
-            Debug.Assert(writeOperations.Count > 0);
-
             var name = modificationCommands[0].TableName;
             var schema = modificationCommands[0].Schema;
 
@@ -173,36 +142,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Update.Internal
                 .Append(SqlGenerationHelper.StatementTerminator).AppendLine()
                 .AppendLine();
 
-            return ResultSetMapping.LastInResultSet;
-        }
-
-        protected override void AppendWhereAffectedClause(
-            StringBuilder commandStringBuilder,
-            IReadOnlyList<IColumnModification> operations)
-        {
-            Check.NotNull(commandStringBuilder, nameof(commandStringBuilder));
-            Check.NotNull(operations, nameof(operations));
-
-            // If a compound key consists of an auto_increment column and a database generated column (e.g. a DEFAULT
-            // value), then we only want to filter by `LAST_INSERT_ID()`, because we can't know what the other generated
-            // values are.
-            // Therefore, we filter out the key columns that are marked as `read`, but are not an auto_increment column,
-            // so that `AppendIdentityWhereCondition()` can safely called for the remaining auto_increment column.
-            // Because we currently use `MySqlValueGenerationStrategy.IdentityColumn` for auto_increment columns as well
-            // as CURRENT_TIMESTAMP columns, we need to use `MySqlPropertyExtensions.IsCompatibleAutoIncrementColumn()`
-            // to ensure, that the column is actually an auto_increment column.
-            // See https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/issues/1300
-            var nonDefaultOperations = operations
-                .Where(
-                    o => !o.IsKey ||
-                         !o.IsRead ||
-                         o.Property == null ||
-                         !o.Property.ValueGenerated.HasFlag(ValueGenerated.OnAdd) ||
-                         MySqlPropertyExtensions.IsCompatibleAutoIncrementColumn(o.Property))
-                .ToList()
-                .AsReadOnly();
-
-            base.AppendWhereAffectedClause(commandStringBuilder, nonDefaultOperations);
+            return ResultSetMapping.LastInResultSet | ResultSetMapping.ResultSetWithRowsAffectedOnly;
         }
 
         protected override void AppendIdentityWhereCondition(StringBuilder commandStringBuilder, IColumnModification columnModification)
@@ -216,5 +156,35 @@ namespace Pomelo.EntityFrameworkCore.MySql.Update.Internal
             => commandStringBuilder
                 .Append("ROW_COUNT() = ")
                 .Append(expectedRowsAffected.ToString(CultureInfo.InvariantCulture));
+
+        protected override bool IsIdentityOperation(IColumnModification modification)
+        {
+            var isIdentityOperation = base.IsIdentityOperation(modification);
+
+            if (isIdentityOperation &&
+                modification.Property is { } property)
+            {
+                var (tableName, schema) = GetTableNameAndSchema(modification, property);
+                var storeObject = StoreObjectIdentifier.Table(tableName, schema);
+
+                return property.GetValueGenerationStrategy(storeObject) is MySqlValueGenerationStrategy.IdentityColumn;
+            }
+
+            return isIdentityOperation;
+        }
+
+        private static (string tableName, string schema) GetTableNameAndSchema(IColumnModification modification, IProperty property)
+        {
+            if (modification.Column?.Table is { } table)
+            {
+                return (table.Name, table.Schema);
+            }
+            else
+            {
+                // CHECK: Is this branch ever hit and then returns something different than null, or can we just rely on
+                // `modification.Column?.Table`?
+                return (property.DeclaringEntityType.GetTableName(), property.DeclaringEntityType.GetSchema());
+            }
+        }
     }
 }
--- test/EFCore.MySql.FunctionalTests/Update/MySqlUpdateSqlGeneratorTest.cs
+++ test/EFCore.MySql.FunctionalTests/Update/MySqlUpdateSqlGeneratorTest.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.EntityFrameworkCore.Update;
+using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
+using Pomelo.EntityFrameworkCore.MySql.Tests;
 using Pomelo.EntityFrameworkCore.MySql.Update.Internal;
 using Xunit;
 
@@ -16,7 +25,8 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Update
     {
         protected override IUpdateSqlGenerator CreateSqlGenerator()
         {
-            var options = new MySqlOptions();
+            var options = GetOptions();
+
             return new MySqlUpdateSqlGenerator(
                 new UpdateSqlGeneratorDependencies(
                     new MySqlSqlGenerationHelper(
@@ -25,11 +35,139 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Update
                     new MySqlTypeMappingSource(
                         TestServiceFactory.Instance.Create<TypeMappingSourceDependencies>(),
                         TestServiceFactory.Instance.Create<RelationalTypeMappingSourceDependencies>(),
-                        options)));
+                        options)),
+                options);
+        }
+
+        private static MySqlOptions GetOptions()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+
+            var extension = new MySqlOptionsExtension()
+                .WithServerVersion(AppConfig.ServerVersion);
+
+            ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+
+            var options = new MySqlOptions();
+            options.Initialize(optionsBuilder.Options);
+            return options;
         }
 
         protected override TestHelpers TestHelpers
             => MySqlTestHelpers.Instance;
+
+        #region Methods, that depend on RETURNING clause or OnAdd
+
+        protected new IModificationCommand CreateInsertCommand(bool identityKey = true, bool isComputed = true, bool defaultsOnly = false)
+        {
+            var model = GetDuckModel();
+            var stateManager = TestHelpers.CreateContextServices(model).GetRequiredService<IStateManager>();
+            var entry = stateManager.GetOrCreateEntry(new Duck());
+            entry.SetEntityState(EntityState.Added);
+            var generator = new ParameterNameGenerator();
+
+            var duckType = model.FindEntityType(typeof(Duck));
+            var idProperty = duckType.FindProperty(nameof(Duck.Id));
+            var nameProperty = duckType.FindProperty(nameof(Duck.Name));
+            var quacksProperty = duckType.FindProperty(nameof(Duck.Quacks));
+            var computedProperty = duckType.FindProperty(nameof(Duck.Computed));
+            var concurrencyProperty = duckType.FindProperty(nameof(Duck.ConcurrencyToken));
+
+            var columnModifications = new[]
+            {
+                new ColumnModificationParameters(
+                    entry, idProperty, idProperty.GetTableColumnMappings().Single().Column, generator.GenerateNext,
+                    idProperty.GetTableColumnMappings().Single().TypeMapping, identityKey, !identityKey, true, false, true),
+                new ColumnModificationParameters(
+                    entry, nameProperty, nameProperty.GetTableColumnMappings().Single().Column, generator.GenerateNext,
+                    nameProperty.GetTableColumnMappings().Single().TypeMapping, false, true, false, false, true),
+                new ColumnModificationParameters(
+                    entry, quacksProperty, quacksProperty.GetTableColumnMappings().Single().Column, generator.GenerateNext,
+                    quacksProperty.GetTableColumnMappings().Single().TypeMapping, false, true, false, false, true),
+                new ColumnModificationParameters(
+                    entry, computedProperty, computedProperty.GetTableColumnMappings().Single().Column, generator.GenerateNext,
+                    computedProperty.GetTableColumnMappings().Single().TypeMapping, isComputed, false, false, false, true),
+                new ColumnModificationParameters(
+                    entry, concurrencyProperty, concurrencyProperty.GetTableColumnMappings().Single().Column, generator.GenerateNext,
+                    concurrencyProperty.GetTableColumnMappings().Single().TypeMapping, false, true, false, false, true)
+            };
+
+            if (defaultsOnly)
+            {
+                columnModifications = columnModifications.Where(c => !c.IsWrite).ToArray();
+            }
+
+            return CreateModificationCommand(entry, columnModifications, false);
+        }
+
+        private IModificationCommand CreateModificationCommand(
+            InternalEntityEntry entry,
+            IReadOnlyList<ColumnModificationParameters> columnModifications,
+            bool sensitiveLoggingEnabled)
+        {
+            var modificationCommandParameters = new ModificationCommandParameters(
+                entry.EntityType.GetTableMappings().Single().Table, sensitiveLoggingEnabled);
+            var modificationCommand = CreateMutableModificationCommandFactory().CreateModificationCommand(
+                modificationCommandParameters);
+
+            modificationCommand.AddEntry(entry, mainEntry: true);
+
+            foreach (var columnModification in columnModifications)
+            {
+                ((INonTrackedModificationCommand)modificationCommand).AddColumnModification(columnModification);
+            }
+
+            return modificationCommand;
+        }
+
+        private IModel GetDuckModel()
+        {
+            var modelBuilder = TestHelpers.CreateConventionBuilder();
+            modelBuilder.Entity<Duck>().ToTable("Ducks", Schema).Property(e => e.Id)/*.ValueGeneratedNever()*/;
+            return modelBuilder.Model.FinalizeModel();
+        }
+
+        public override void AppendInsertOperation_for_only_identity()
+        {
+            var stringBuilder = new StringBuilder();
+            var command = CreateInsertCommand(true, false);
+
+            CreateSqlGenerator().AppendInsertOperation(stringBuilder, command, 0);
+
+            AppendInsertOperation_for_only_identity_verification(stringBuilder);
+        }
+
+        public override void AppendInsertOperation_for_all_store_generated_columns()
+        {
+            var stringBuilder = new StringBuilder();
+            var command = CreateInsertCommand(true, true, true);
+
+            CreateSqlGenerator().AppendInsertOperation(stringBuilder, command, 0);
+
+            AppendInsertOperation_for_all_store_generated_columns_verification(stringBuilder);
+        }
+
+        public override void AppendInsertOperation_for_only_single_identity_columns()
+        {
+            var stringBuilder = new StringBuilder();
+            var command = CreateInsertCommand(identityKey: true, isComputed: false, defaultsOnly: true);
+
+            CreateSqlGenerator().AppendInsertOperation(stringBuilder, command, 0);
+
+            AppendInsertOperation_for_only_single_identity_columns_verification(stringBuilder);
+        }
+
+        public override void AppendInsertOperation_insert_if_store_generated_columns_exist()
+        {
+            var stringBuilder = new StringBuilder();
+            var command = CreateInsertCommand();
+
+            CreateSqlGenerator().AppendInsertOperation(stringBuilder, command, 0);
+
+            AppendInsertOperation_insert_if_store_generated_columns_exist_verification(stringBuilder);
+        }
+
+        #endregion
 
         [ConditionalFact]
         public void AppendBulkInsertOperation_appends_insert_if_store_generated_columns_exist()
@@ -112,66 +250,66 @@ WHERE ROW_COUNT() = 1 AND `Id` = LAST_INSERT_ID();
             var sqlGenerator = (IMySqlUpdateSqlGenerator)CreateSqlGenerator();
             var grouping = sqlGenerator.AppendBulkInsertOperation(stringBuilder, new[] { command, command }, 0, out _);
 
-            var expectedText = @"INSERT INTO "
-                               + SchemaPrefix
-                               + OpenDelimiter
-                               + "Ducks"
-                               + CloseDelimiter
-                               + NoColumns
-                               + Environment.NewLine
-                               + DefaultValues
-                               + ";"
-                               + Environment.NewLine;
+            var expectedText = @"INSERT INTO `Ducks` ()
+VALUES (),
+();
+";
             AssertBaseline(
-                expectedText + expectedText,
+                expectedText,
                 stringBuilder.ToString());
             Assert.Equal(ResultSetMapping.NoResults, grouping);
         }
 
-        // TODO: Adjust for MySQL.
         protected override void AppendDeleteOperation_creates_full_delete_command_text_verification(StringBuilder stringBuilder)
             => AssertBaseline(
-                @"DELETE FROM [dbo].[Ducks]
-OUTPUT 1
-WHERE [Id] = @p0;
+                @"DELETE FROM `Ducks`
+WHERE `Id` = @p0;
+SELECT ROW_COUNT();
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendDeleteOperation_creates_full_delete_command_text_with_concurrency_check_verification(
             StringBuilder stringBuilder)
             => AssertBaseline(
-                @"DELETE FROM [dbo].[Ducks]
-OUTPUT 1
-WHERE [Id] = @p0 AND [ConcurrencyToken] IS NULL;
+                @"DELETE FROM `Ducks`
+WHERE `Id` = @p0 AND `ConcurrencyToken` IS NULL;
+SELECT ROW_COUNT();
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendInsertOperation_insert_if_store_generated_columns_exist_verification(StringBuilder stringBuilder)
             => AssertBaseline(
-                @"INSERT INTO [dbo].[Ducks] ([Name], [Quacks], [ConcurrencyToken])
-OUTPUT INSERTED.[Id], INSERTED.[Computed]
+                @"INSERT INTO `Ducks` (`Name`, `Quacks`, `ConcurrencyToken`)
 VALUES (@p0, @p1, @p2);
+SELECT `Id`, `Computed`
+FROM `Ducks`
+WHERE ROW_COUNT() = 1 AND `Id` = LAST_INSERT_ID();
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendInsertOperation_for_store_generated_columns_but_no_identity_verification(
             StringBuilder stringBuilder)
             => AssertBaseline(
-                @"INSERT INTO [dbo].[Ducks] ([Id], [Name], [Quacks], [ConcurrencyToken])
-OUTPUT INSERTED.[Computed]
+                @"INSERT INTO `Ducks` (`Id`, `Name`, `Quacks`, `ConcurrencyToken`)
 VALUES (@p0, @p1, @p2, @p3);
+SELECT `Computed`
+FROM `Ducks`
+WHERE ROW_COUNT() = 1 AND `Id` = @p0;
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendInsertOperation_for_only_identity_verification(StringBuilder stringBuilder)
             => AssertBaseline(
-                @"INSERT INTO [dbo].[Ducks] ([Name], [Quacks], [ConcurrencyToken])
-OUTPUT INSERTED.[Id]
+                @"INSERT INTO `Ducks` (`Name`, `Quacks`, `ConcurrencyToken`)
 VALUES (@p0, @p1, @p2);
+SELECT `Id`
+FROM `Ducks`
+WHERE ROW_COUNT() = 1 AND `Id` = LAST_INSERT_ID();
+
 ",
                 stringBuilder.ToString());
 
@@ -253,41 +391,45 @@ VALUES (@p0, @p1, @p2);
                 stringBuilder.ToString());
         }
 
-        // TODO: Adjust for MySQL.
         protected override void AppendUpdateOperation_if_store_generated_columns_exist_verification(
             StringBuilder stringBuilder)
             => AssertBaseline(
-                @"UPDATE [dbo].[Ducks] SET [Name] = @p0, [Quacks] = @p1, [ConcurrencyToken] = @p2
-OUTPUT INSERTED.[Computed]
-WHERE [Id] = @p3 AND [ConcurrencyToken] IS NULL;
+                @"UPDATE `Ducks` SET `Name` = @p0, `Quacks` = @p1, `ConcurrencyToken` = @p2
+WHERE `Id` = @p3 AND `ConcurrencyToken` IS NULL;
+SELECT `Computed`
+FROM `Ducks`
+WHERE ROW_COUNT() = 1 AND `Id` = @p3;
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendUpdateOperation_if_store_generated_columns_dont_exist_verification(
             StringBuilder stringBuilder)
             => AssertBaseline(
-                @"UPDATE [dbo].[Ducks] SET [Name] = @p0, [Quacks] = @p1, [ConcurrencyToken] = @p2
-OUTPUT 1
-WHERE [Id] = @p3;
+                @"UPDATE `Ducks` SET `Name` = @p0, `Quacks` = @p1, `ConcurrencyToken` = @p2
+WHERE `Id` = @p3;
+SELECT ROW_COUNT();
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendUpdateOperation_appends_where_for_concurrency_token_verification(StringBuilder stringBuilder)
             => AssertBaseline(
-                @"UPDATE [dbo].[Ducks] SET [Name] = @p0, [Quacks] = @p1, [ConcurrencyToken] = @p2
-OUTPUT 1
-WHERE [Id] = @p3 AND [ConcurrencyToken] IS NULL;
+                @"UPDATE `Ducks` SET `Name` = @p0, `Quacks` = @p1, `ConcurrencyToken` = @p2
+WHERE `Id` = @p3 AND `ConcurrencyToken` IS NULL;
+SELECT ROW_COUNT();
+
 ",
                 stringBuilder.ToString());
 
-        // TODO: Adjust for MySQL.
         protected override void AppendUpdateOperation_for_computed_property_verification(StringBuilder stringBuilder)
             => AssertBaseline(
-                @"UPDATE [dbo].[Ducks] SET [Name] = @p0, [Quacks] = @p1, [ConcurrencyToken] = @p2
-OUTPUT INSERTED.[Computed]
-WHERE [Id] = @p3;
+                @"UPDATE `Ducks` SET `Name` = @p0, `Quacks` = @p1, `ConcurrencyToken` = @p2
+WHERE `Id` = @p3;
+SELECT `Computed`
+FROM `Ducks`
+WHERE ROW_COUNT() = 1 AND `Id` = @p3;
+
 ",
                 stringBuilder.ToString());
 
@@ -313,8 +455,6 @@ WHERE [Id] = @p3;
             => "`";
 
         private void AssertBaseline(string expected, string actual)
-        {
-            Assert.Equal(expected, actual, ignoreLineEndingDifferences: true);
-        }
+            => Assert.Equal(expected.TrimEnd(), actual.TrimEnd(), ignoreLineEndingDifferences: true);
     }
 }
