649ffb80e7decb18428b605ce8eb9b34e0535773
Laurents Meyer <laucomm@gmail.com>
2024-03-06 16:58:46 +0100

    Use current DbConnection for updating the `sql_mode`. (#1862)
    Don't set `sql_mode` for admin/master connections.
    
    (cherry picked from commit 2b0ca144fa80f07d4fae363bc4b7e588217f400f)

--- test/EFCore.MySql.FunctionalTests/ConnectionMySqlTest.cs
+++ test/EFCore.MySql.FunctionalTests/ConnectionMySqlTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using MySqlConnector;
 using Pomelo.EntityFrameworkCore.MySql.FunctionalTests.TestUtilities;
@@ -111,6 +112,32 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests
             Assert.Equal(string.Empty, new MySqlConnectionStringBuilder(masterConnection.ConnectionString).Database);
 
             masterConnection.Open();
+        }
+
+        [Fact]
+        public void Can_create_database_with_disablebackslashescaping()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<GeneralOptionsContext>();
+            optionsBuilder.UseMySql(MySqlTestStore.CreateConnectionString("ConnectionTest_" + Guid.NewGuid()), AppConfig.ServerVersion, b => b.ApplyConfiguration().DisableBackslashEscaping());
+            using var context = new GeneralOptionsContext(optionsBuilder.Options);
+
+            var relationalDatabaseCreator = context.GetService<IRelationalDatabaseCreator>();
+
+            try
+            {
+                relationalDatabaseCreator.EnsureCreated();
+            }
+            finally
+            {
+                try
+                {
+                    relationalDatabaseCreator.EnsureDeleted();
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
         }
 
         private readonly IServiceProvider _serviceProvider = new ServiceCollection()
