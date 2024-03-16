e3e00369513c0a8ce76bc2f7253a8912fea26bda
Laurents Meyer <laucomm@gmail.com>
2024-03-16 21:26:21 +0100

    Prepare release 6.0.3 (#1888)
    * Prepare release 6.0.3.
    
    * Fix tests.

--- test/EFCore.MySql.FunctionalTests/Query/FromSqlQueryMySqlTest.cs
+++ test/EFCore.MySql.FunctionalTests/Query/FromSqlQueryMySqlTest.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using MySqlConnector;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Tests.TestUtilities.Attributes;
 using Xunit;
 
 namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
@@ -12,6 +14,12 @@ namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.Query
         public FromSqlQueryMySqlTest(NorthwindQueryMySqlFixture<NoopModelCustomizer> fixture)
             : base(fixture)
         {
+        }
+
+        [SupportedServerVersionCondition(nameof(ServerVersionSupport.ExceptIntercept))]
+        public override Task Multiple_occurrences_of_FromSql_with_db_parameter_adds_parameter_only_once(bool async)
+        {
+            return base.Multiple_occurrences_of_FromSql_with_db_parameter_adds_parameter_only_once(async);
         }
 
         protected override DbParameter CreateDbParameter(string name, object value)
