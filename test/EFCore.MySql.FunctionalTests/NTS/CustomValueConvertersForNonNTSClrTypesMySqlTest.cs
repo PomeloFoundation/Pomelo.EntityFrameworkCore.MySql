PomeloFoundation/Pomelo.EntityFrameworkCore.MySql

commit 3ef628ff7dfc38b8a4a8075fd9d7c5677f48829a

Update to EF Core 8.0.1 (#1852)
* Update dependencies.

* Update tests for EF Core 8.0.1.

* Fix NTS source type mappings.

* Implement MySqlRuntimeModelConvention and MySqlCSharpRuntimeAnnotationCodeGenerator. Update model extension methods.

* Fix identity columns.

* Improve test utilities.

* Refactor MySqlParameterInliningExpressionVisitor.

* Adjust TransactionMySqlTest.

* Ensure different default database names for Northwind databases with different collations.

* Correct MySqlBug96947Workaround version range.

Dependencies.targets
@@ -1,6 +1,6 @@
 <Project>
   <PropertyGroup Label="Common Versions">
-    <EFCoreVersion>[8.0.0,8.0.999]</EFCoreVersion>
+    <EFCoreVersion>[8.0.1,8.0.999]</EFCoreVersion>
   </PropertyGroup>
 
   <ItemGroup Label="Dependencies">
@@ -13,16 +13,16 @@
     <PackageReference Update="MySqlConnector.DependencyInjection" Version="2.3.5" />
 
     <PackageReference Update="NetTopologySuite" Version="2.5.0" />
-    <PackageReference Update="System.Text.Json" Version="8.0.0" />
+    <PackageReference Update="System.Text.Json" Version="8.0.1" />
     <PackageReference Update="Newtonsoft.Json" Version="13.0.3" />
 
     <PackageReference Update="Castle.Core" Version="5.1.1" />
-    <PackageReference Update="Microsoft.AspNetCore.Identity.EntityFrameworkCore" Version="8.0.0" />
-    <PackageReference Update="Microsoft.AspNetCore.Mvc.NewtonsoftJson" Version="8.0.0" />
+    <PackageReference Update="Microsoft.AspNetCore.Identity.EntityFrameworkCore" Version="8.0.1" />
+    <PackageReference Update="Microsoft.AspNetCore.Mvc.NewtonsoftJson" Version="8.0.1" />
     <PackageReference Update="Microsoft.Bcl.AsyncInterfaces" Version="8.0.0" />
     <PackageReference Update="Microsoft.Bcl.HashCode" Version="1.1.1" />
     <PackageReference Update="Microsoft.Extensions.Caching.Memory" Version="8.0.0" />
-    <PackageReference Update="Microsoft.Extensions.Configuration.Binder" Version="8.0.0" />
+    <PackageReference Update="Microsoft.Extensions.Configuration.Binder" Version="8.0.1" />
     <PackageReference Update="Microsoft.Extensions.Configuration.EnvironmentVariables" Version="8.0.0" />
     <PackageReference Update="Microsoft.Extensions.Configuration.FileExtensions" Version="8.0.0" />
     <PackageReference Update="Microsoft.Extensions.Configuration.Json" Version="8.0.0" />

src/EFCore.MySql.NTS/Storage/Internal/MySqlNetTopologySuiteTypeMappingSourcePlugin.cs
@@ -48,19 +48,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
             { "multipolygon", typeof(MultiPolygon) },             // geometry -> geometrycollection -> multisurface -> multipolygon
         };
 
-        private static readonly Dictionary<Type, string> _spatialClrTypeMappings = new Dictionary<Type, string>
-        {
-            { typeof(Geometry), "geometry" },                     // geometry
-            { typeof(Point), "point" },                           // geometry -> point
-            { typeof(LineString), "linestring" },                 // geometry -> curve -> linestring
-            { typeof(LinearRing), "linearring" },                 // geometry -> curve -> linestring -> linearring
-            { typeof(Polygon), "polygon" },                       // geometry -> surface -> polygon
-            { typeof(GeometryCollection), "geometrycollection" }, // geometry -> geometrycollection
-            { typeof(MultiPoint), "multipoint" },                 // geometry -> geometrycollection -> multipoint
-            { typeof(MultiLineString), "multilinestring" },       // geometry -> geometrycollection -> multicurve -> multilinestring
-            { typeof(MultiPolygon), "multipolygon" },             // geometry -> geometrycollection -> multisurface -> multipolygon
-        };
-
         private readonly NtsGeometryServices _geometryServices;
         private readonly IMySqlOptions _options;
 
@@ -89,72 +76,75 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
         public virtual RelationalTypeMapping FindMapping(in RelationalTypeMappingInfo mappingInfo)
         {
             var clrType = mappingInfo.ClrType;
-            var storeTypeName = mappingInfo.StoreTypeName;
-            var storeTypeNameBase = mappingInfo.StoreTypeNameBase;
+            var storeTypeName = mappingInfo.StoreTypeName?.ToLowerInvariant();
+            string defaultStoreType = null;
+            Type defaultClrType = null;
 
-            if (storeTypeName != null)
-            {
-                // First look for the fully qualified store type name.
-                if (_spatialStoreTypeMappings.TryGetValue(storeTypeName, out var mappedClrType))
-                {
-                    // We found the user-specified store type.
-                    // If no CLR type was provided, we're probably scaffolding from an existing database. Take the first
-                    // mapping as the default.
-                    // If a CLR type was provided, look for a mapping between the store and CLR types. If none is found,
-                    // fail immediately.
-                    clrType = clrType == null
-                        ? mappedClrType
-                        : clrType.IsAssignableFrom(mappedClrType)
-                            ? clrType
-                            : null;
-
-                    return clrType == null
-                        ? null
-                        : (RelationalTypeMapping)Activator.CreateInstance(
-                            typeof(MySqlGeometryTypeMapping<>).MakeGenericType(clrType),
-                            _geometryServices,
-                            storeTypeName,
-                            _options);
-                }
-
-                // Then look for the base store type name.
-                if (_spatialStoreTypeMappings.TryGetValue(storeTypeNameBase, out mappedClrType))
-                {
-                    clrType = clrType == null
-                        ? mappedClrType
-                        : clrType.IsAssignableFrom(mappedClrType)
-                            ? clrType
-                            : null;
-
-                    if (clrType == null)
-                    {
-                        return null;
-                    }
-
-                    var typeMapping = (RelationalTypeMapping)Activator.CreateInstance(
-                        typeof(MySqlGeometryTypeMapping<>).MakeGenericType(clrType),
-                        _geometryServices,
-                        storeTypeName,
-                        _options);
-
-                    return typeMapping.Clone(mappingInfo);
-                }
-
-                // A store type name was provided, but is unknown. This could be a domain (alias) type, in which case
-                // we proceed with a CLR type lookup (if the type doesn't exist at all the failure will come later).
-            }
-
-            if (clrType != null &&
-                _spatialClrTypeMappings.TryGetValue(clrType, out var mappedStoreTypeName))
-            {
-                return (RelationalTypeMapping)Activator.CreateInstance(
-                    typeof(MySqlGeometryTypeMapping<>).MakeGenericType(clrType),
+            return (clrType != null
+                    && TryGetDefaultStoreType(clrType, out defaultStoreType))
+                   || (storeTypeName != null
+                       && _spatialStoreTypeMappings.TryGetValue(storeTypeName, out defaultClrType))
+                ? (RelationalTypeMapping)Activator.CreateInstance(
+                    typeof(MySqlGeometryTypeMapping<>).MakeGenericType(clrType ?? defaultClrType ?? typeof(Geometry)),
                     _geometryServices,
-                    mappedStoreTypeName,
-                    _options);
+                    storeTypeName ?? defaultStoreType ?? "geometry",
+                    _options)
+                : null;
+        }
+
+        private static bool TryGetDefaultStoreType(Type type, out string defaultStoreType)
+        {
+            // geometry -> geometrycollection -> multisurface -> multipolygon
+            if (typeof(MultiPolygon).IsAssignableFrom(type))
+            {
+                defaultStoreType = "multipolygon";
+            }
+            // geometry -> geometrycollection -> multicurve -> multilinestring
+            else if (typeof(MultiLineString).IsAssignableFrom(type))
+            {
+                defaultStoreType = "multilinestring";
+            }
+            // geometry -> geometrycollection -> multipoint
+            else if (typeof(MultiPoint).IsAssignableFrom(type))
+            {
+                defaultStoreType = "multipoint";
+            }
+            // geometry -> geometrycollection
+            else if (typeof(GeometryCollection).IsAssignableFrom(type))
+            {
+                defaultStoreType = "geometrycollection";
+            }
+            // geometry -> surface -> polygon
+            else if (typeof(Polygon).IsAssignableFrom(type))
+            {
+                defaultStoreType = "polygon";
+            }
+            // geometry -> curve -> linestring -> linearring
+            else if (typeof(LinearRing).IsAssignableFrom(type))
+            {
+                defaultStoreType = "linearring";
+            }
+            // geometry -> curve -> linestring
+            else if (typeof(LineString).IsAssignableFrom(type))
+            {
+                defaultStoreType = "linestring";
+            }
+            // geometry -> point
+            else if (typeof(Point).IsAssignableFrom(type))
+            {
+                defaultStoreType = "point";
+            }
+            // geometry
+            else if (typeof(Geometry).IsAssignableFrom(type))
+            {
+                defaultStoreType = "geometry";
+            }
+            else
+            {
+                defaultStoreType = null;
             }
 
-            return null;
+            return defaultStoreType != null;
         }
     }
 }

src/EFCore.MySql.NTS/Query/Internal/MySqlGeometryMethodTranslator.cs
@@ -127,7 +127,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Query.Internal
                                 _sqlExpressionFactory.Constant(1))
                         },
                         method.ReturnType,
-                        _typeMappingSource.FindMapping(method.ReturnType, storeType),
+                        _typeMappingSource.FindMapping(method.ReturnType),
                         false);
                 }
 
@@ -137,7 +137,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Query.Internal
                         instance,
                         arguments[0],
                         method.ReturnType,
-                        _typeMappingSource.FindMapping(method.ReturnType, storeType));
+                        _typeMappingSource.FindMapping(method.ReturnType));
                 }
 
                 if (Equals(method, _isWithinDistance))

src/EFCore.MySql/Design/Internal/MySqlAnnotationCodeGenerator.cs
@@ -4,9 +4,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
-using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -19,6 +19,11 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
 {
     public class MySqlAnnotationCodeGenerator : AnnotationCodeGenerator
     {
+        private static readonly MethodInfo _modelUseIdentityColumnsMethodInfo
+            = typeof(MySqlModelBuilderExtensions).GetRequiredRuntimeMethod(
+                nameof(MySqlModelBuilderExtensions.AutoIncrementColumns),
+                typeof(ModelBuilder));
+
         private static readonly MethodInfo _modelHasCharSetMethodInfo
             = typeof(MySqlModelBuilderExtensions).GetRequiredRuntimeMethod(
                 nameof(MySqlModelBuilderExtensions.HasCharSet),
@@ -39,6 +44,12 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 typeof(ModelBuilder),
                 typeof(string));
 
+        private static readonly MethodInfo _modelHasAnnotationMethodInfo
+            = typeof(ModelBuilder).GetRequiredRuntimeMethod(
+                nameof(ModelBuilder.HasAnnotation),
+                typeof(string),
+                typeof(object));
+
         private static readonly MethodInfo _entityTypeHasCharSetMethodInfo
             = typeof(MySqlEntityTypeBuilderExtensions).GetRequiredRuntimeMethod(
                 nameof(MySqlEntityTypeBuilderExtensions.HasCharSet),
@@ -53,13 +64,23 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 typeof(string),
                 typeof(DelegationModes?));
 
+        private static readonly MethodInfo _propertyUseIdentityColumnMethodInfo
+            = typeof(MySqlPropertyBuilderExtensions).GetRequiredRuntimeMethod(
+                nameof(MySqlPropertyBuilderExtensions.UseMySqlIdentityColumn),
+                typeof(PropertyBuilder));
+
+        private static readonly MethodInfo _propertyUseComputedColumnMethodInfo
+            = typeof(MySqlPropertyBuilderExtensions).GetRequiredRuntimeMethod(
+                nameof(MySqlPropertyBuilderExtensions.UseMySqlComputedColumn),
+                typeof(PropertyBuilder));
+
         private static readonly MethodInfo _propertyHasCharSetMethodInfo
             = typeof(MySqlPropertyBuilderExtensions).GetRequiredRuntimeMethod(
                 nameof(MySqlPropertyBuilderExtensions.HasCharSet),
                 typeof(PropertyBuilder),
                 typeof(string));
 
-        public MySqlAnnotationCodeGenerator([NotNull] AnnotationCodeGeneratorDependencies dependencies)
+        public MySqlAnnotationCodeGenerator([JetBrains.Annotations.NotNull] AnnotationCodeGeneratorDependencies dependencies)
             : base(dependencies)
         {
         }
@@ -94,7 +115,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 var delegationModes = model[MySqlAnnotationNames.CharSetDelegation] as DelegationModes?;
                 return new MethodCallCodeFragment(
                     _modelHasCharSetMethodInfo,
-                    new[] {annotation.Value}
+                    new[] { annotation.Value }
                         .AppendIfTrue(delegationModes.HasValue, delegationModes)
                         .ToArray());
             }
@@ -115,7 +136,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 var delegationModes = model[MySqlAnnotationNames.CollationDelegation] as DelegationModes?;
                 return new MethodCallCodeFragment(
                     _modelUseCollationMethodInfo,
-                    new[] {annotation.Value}
+                    new[] { annotation.Value }
                         .AppendIfTrue(delegationModes.HasValue, delegationModes)
                         .ToArray());
             }
@@ -146,7 +167,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 var delegationModes = entityType[MySqlAnnotationNames.CharSetDelegation] as DelegationModes?;
                 return new MethodCallCodeFragment(
                     _entityTypeHasCharSetMethodInfo,
-                    new[] {annotation.Value}
+                    new[] { annotation.Value }
                         .AppendIfTrue(delegationModes.HasValue, delegationModes)
                         .ToArray());
             }
@@ -165,7 +186,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 var delegationModes = entityType[MySqlAnnotationNames.CollationDelegation] as DelegationModes?;
                 return new MethodCallCodeFragment(
                     _entityTypeUseCollationMethodInfo,
-                    new[] {annotation.Value}
+                    new[] { annotation.Value }
                         .AppendIfTrue(delegationModes.HasValue, delegationModes)
                         .ToArray());
             }
@@ -192,7 +213,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 var delegationModes = entityType[MySqlAnnotationNames.CharSetDelegation] as DelegationModes?;
                 return new AttributeCodeFragment(
                     typeof(MySqlCharSetAttribute),
-                    new[] {annotation.Value}
+                    new[] { annotation.Value }
                         .AppendIfTrue(delegationModes.HasValue, delegationModes)
                         .ToArray());
             }
@@ -211,7 +232,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
                 var delegationModes = entityType[MySqlAnnotationNames.CollationDelegation] as DelegationModes?;
                 return new AttributeCodeFragment(
                     typeof(MySqlCollationAttribute),
-                    new[] {annotation.Value}
+                    new[] { annotation.Value }
                         .AppendIfTrue(delegationModes.HasValue, delegationModes)
                         .ToArray());
             }
@@ -240,7 +261,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
 
             switch (annotation.Name)
             {
-                case MySqlAnnotationNames.CharSet when annotation.Value is string {Length: > 0} charSet:
+                case MySqlAnnotationNames.CharSet when annotation.Value is string { Length: > 0 } charSet:
                     return new MethodCallCodeFragment(
                         _propertyHasCharSetMethodInfo,
                         charSet);
@@ -257,10 +278,74 @@ namespace Pomelo.EntityFrameworkCore.MySql.Design.Internal
 
             return annotation.Name switch
             {
-                MySqlAnnotationNames.CharSet when annotation.Value is string {Length: > 0} charSet => new AttributeCodeFragment(typeof(MySqlCharSetAttribute), charSet),
-                RelationalAnnotationNames.Collation when annotation.Value is string {Length: > 0} collation => new AttributeCodeFragment(typeof(MySqlCollationAttribute), collation),
+                MySqlAnnotationNames.CharSet when annotation.Value is string { Length: > 0 } charSet => new AttributeCodeFragment(
+                    typeof(MySqlCharSetAttribute), charSet),
+                RelationalAnnotationNames.Collation when annotation.Value is string { Length: > 0 } collation => new AttributeCodeFragment(
+                    typeof(MySqlCollationAttribute), collation),
                 _ => base.GenerateDataAnnotation(property, annotation)
             };
+        }
+
+        public override IReadOnlyList<MethodCallCodeFragment> GenerateFluentApiCalls(
+            IModel model,
+            IDictionary<string, IAnnotation> annotations)
+        {
+            var fragments = new List<MethodCallCodeFragment>(base.GenerateFluentApiCalls(model, annotations));
+
+            if (GenerateValueGenerationStrategy(annotations, onModel: true) is { } valueGenerationStrategy)
+            {
+                fragments.Add(valueGenerationStrategy);
+            }
+
+            return fragments;
+        }
+
+        public override IReadOnlyList<MethodCallCodeFragment> GenerateFluentApiCalls(
+            IProperty property,
+            IDictionary<string, IAnnotation> annotations)
+        {
+            var fragments = new List<MethodCallCodeFragment>(base.GenerateFluentApiCalls(property, annotations));
+
+            if (GenerateValueGenerationStrategy(annotations, onModel: false) is { } valueGenerationStrategy)
+            {
+                fragments.Add(valueGenerationStrategy);
+            }
+
+            return fragments;
+        }
+
+        private MethodCallCodeFragment GenerateValueGenerationStrategy(IDictionary<string, IAnnotation> annotations, bool onModel)
+            => TryGetAndRemove(annotations, MySqlAnnotationNames.ValueGenerationStrategy, out MySqlValueGenerationStrategy strategy)
+                ? strategy switch
+                {
+                    MySqlValueGenerationStrategy.IdentityColumn => new MethodCallCodeFragment(
+                        onModel
+                            ? _modelUseIdentityColumnsMethodInfo
+                            : _propertyUseIdentityColumnMethodInfo),
+                    MySqlValueGenerationStrategy.ComputedColumn => new MethodCallCodeFragment(_propertyUseComputedColumnMethodInfo),
+                    MySqlValueGenerationStrategy.None => new MethodCallCodeFragment(
+                        _modelHasAnnotationMethodInfo,
+                        MySqlAnnotationNames.ValueGenerationStrategy,
+                        MySqlValueGenerationStrategy.None),
+                    _ => throw new ArgumentOutOfRangeException(strategy.ToString())
+                }
+                : null;
+
+        private static bool TryGetAndRemove<T>(
+            IDictionary<string, IAnnotation> annotations,
+            string annotationName,
+            [NotNullWhen(true)] out T annotationValue)
+        {
+            if (annotations.TryGetValue(annotationName, out var annotation)
+                && annotation.Value is not null)
+            {
+                annotations.Remove(annotationName);
+                annotationValue = (T)annotation.Value;
+                return true;
+            }
+
+            annotationValue = default;
+            return false;
         }
     }
 }

commit 9dbad69834722487f9d1a30a8b4a609295699a53

Fix regression to allow custom value conversion mapping when the ClrType is of non-NTS type. plus added unit test.  - regression introduced in commit [3ef628f](https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/commit/3ef628ff7dfc38b8a4a8075fd9d7c5677f48829a#diff-4f1d25d8c94e4764d1beef032d7ecc17efaf6a2f592db3dab748bac1d7fc0446R87)
Refs #1921

Dependencies.targets
@@ -46,6 +46,7 @@
     <PackageReference Update="DotNetAnalyzers.DocumentationAnalyzers" Version="1.0.0-beta.59" />
     <PackageReference Update="StyleCop.Analyzers" Version="1.1.118" PrivateAssets="All" />
     <PackageReference Update="Microsoft.CodeAnalysis" Version="4.5.0" />
+    <PackageReference Update="Shouldly" Version="4.0.3" />
     <PackageReference Update="Microsoft.CodeAnalysis.Features" Version="4.5.0" />
     <PackageReference Update="Microsoft.CodeAnalysis.CSharp.Features" Version="4.5.0" />
     <PackageReference Update="Microsoft.CodeAnalysis.VisualBasic.Features" Version="4.5.0" />

src/EFCore.MySql.NTS/Storage/Internal/MySqlNetTopologySuiteTypeMappingSourcePlugin.cs
@@ -80,16 +80,30 @@ namespace Pomelo.EntityFrameworkCore.MySql.Storage.Internal
             string defaultStoreType = null;
             Type defaultClrType = null;
 
-            return (clrType != null
-                    && TryGetDefaultStoreType(clrType, out defaultStoreType))
-                   || (storeTypeName != null
-                       && _spatialStoreTypeMappings.TryGetValue(storeTypeName, out defaultClrType))
-                ? (RelationalTypeMapping)Activator.CreateInstance(
+            var hasDefaultStoreType = clrType != null
+                    && TryGetDefaultStoreType(clrType, out defaultStoreType);
+            var hasDefaultClrType = storeTypeName != null
+                       && _spatialStoreTypeMappings.TryGetValue(storeTypeName, out defaultClrType);
+
+            if (!(hasDefaultStoreType || hasDefaultClrType))
+            {
+                return null;
+            }
+
+            // NOTE: If the incoming user-specified 'clrType' is of the known calculated 'defaultClrType', ONLY then proceeed
+            // with the creation of 'MySqlGeometryTypeMapping'.
+            clrType = clrType == null
+                ? defaultClrType
+                : clrType.IsAssignableFrom(defaultClrType)
+                    ? clrType
+                    : null;
+            return clrType == null
+                ? null
+                : (RelationalTypeMapping)Activator.CreateInstance(
                     typeof(MySqlGeometryTypeMapping<>).MakeGenericType(clrType ?? defaultClrType ?? typeof(Geometry)),
                     _geometryServices,
                     storeTypeName ?? defaultStoreType ?? "geometry",
-                    _options)
-                : null;
+                    _options);
         }
 
         private static bool TryGetDefaultStoreType(Type type, out string defaultStoreType)

test/EFCore.MySql.FunctionalTests/EFCore.MySql.FunctionalTests.csproj
@@ -32,6 +32,7 @@
     <PackageReference Include="Microsoft.Extensions.Configuration.EnvironmentVariables" />
     <PackageReference Include="Microsoft.Extensions.Configuration.FileExtensions" />
     <PackageReference Include="Microsoft.Extensions.Configuration.Json" />
+    <PackageReference Include="Shouldly" />
   </ItemGroup>
 
   <ItemGroup Condition="'$(LocalEFCoreRepository)' == ''">

test/EFCore.MySql.FunctionalTests/NTS/CustomValueConvertersForNonNTSClrTypesMySqlTest.cs
@@ -1,0 +1,96 @@
+﻿namespace Pomelo.EntityFrameworkCore.MySql.FunctionalTests.NTS;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NetTopologySuite.Geometries;
+using Pomelo.EntityFrameworkCore.MySql.Tests;
+using Shouldly;
+using Xunit;
+
+public class CustomValueConvertersForNonNTSClrTypesMySqlTest
+{
+    [Fact]
+    public async Task NonNTS_ClrType_CanBe_Mapped_to_CustomValueConverters()
+    {
+        var services = new ServiceCollection()
+            .AddDbContext<CustomDbContext>()
+            .AddSingleton(
+                new DbContextOptionsBuilder<CustomDbContext>()
+                    .UseMySql(
+                        AppConfig.ConnectionString,
+                        AppConfig.ServerVersion,
+                        options =>
+                        {
+                            options.UseNetTopologySuite();
+                        })
+                    .EnableSensitiveDataLogging()
+                    .LogTo(Console.WriteLine, LogLevel.Debug)
+                .Options);
+
+        var provider = services.BuildServiceProvider();
+        await using var context = provider.GetRequiredService<CustomDbContext>();
+
+        // Validate `MySqlNetTopologySuiteTypeMappingSourcePlugin.FindMapping()` doesn't throw.
+        await Should.NotThrowAsync(context.Database.EnsureDeletedAsync());
+        await Should.NotThrowAsync(context.Database.EnsureCreatedAsync());
+    }
+}
+
+public sealed class CustomDbContext(DbContextOptions<CustomDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var testClass = modelBuilder.Entity<TestClass>();
+        testClass.Property(t => t.Vertices)
+            .HasColumnType("GEOMETRY")
+            .HasConversion(new MysqlGeometryWkbValueConverter());
+
+        var testClass2 = modelBuilder.Entity<TestClass2>();
+        testClass2.Property(t => t.Vertices)
+            .HasColumnType("GEOMETRY");
+    }
+}
+
+public class TestClass
+{
+    public int Id { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public byte[] Vertices { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+}
+
+public class TestClass2
+{
+    public int Id { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Geometry Vertices { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+}
+
+/// <summary>
+///   MySql's internal geometry format is WKB with an initial 4
+///   bytes for the SRID:
+///   https://dev.mysql.com/doc/refman/5.7/en/gis-data-formats.html
+/// </summary>
+public class MysqlGeometryWkbValueConverter : ValueConverter<byte[], byte[]>
+{
+    public MysqlGeometryWkbValueConverter()
+        : base(
+              clr => AddSRID(clr),
+              col => StripSRID(col))
+    {
+    }
+
+    private static byte[] AddSRID(byte[] wkb) =>
+        new byte[] { 0, 0, 0, 0, }.Concat(wkb).ToArray();
+
+    private static byte[] StripSRID(byte[] col) =>
+        col.Skip(4).ToArray();
+}